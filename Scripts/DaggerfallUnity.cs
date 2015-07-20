51d34e73432928f908c13209a4b670f7ff84715d
Interkarma <interkarma@dfworkshop.net>
2015-07-20 16:00:10 +1000

    Fixing version to 1.4.3 post-merge.

--- Scripts/DaggerfallUnity.cs
+++ Scripts/DaggerfallUnity.cs
@@ -37,7 +37,7 @@ namespace DaggerfallWorkshop
     public class DaggerfallUnity : MonoBehaviour
     {
         [NonSerialized]
-        public const string Version = "1.3.31";
+        public const string Version = "1.4.3";
 
         #region Fields
 
