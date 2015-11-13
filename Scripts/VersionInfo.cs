94d6ed2b00dd2bbd5ee608fea9adaa3869d2e217
Interkarma <interkarma@dfworkshop.net>
2015-11-14 06:52:02 +1000

    Bumping version for 0.1 (first non-test release).

--- Scripts/VersionInfo.cs
+++ Scripts/VersionInfo.cs
@@ -13,6 +13,6 @@ using System;
 
 public static class VersionInfo
 {
-    public const string DaggerfallToolsForUnityVersion = "1.4.49";
-    public const string DaggerfallUnityVersion = "0.0.7";
+    public const string DaggerfallToolsForUnityVersion = "1.4.52";
+    public const string DaggerfallUnityVersion = "0.1.0";
 }
