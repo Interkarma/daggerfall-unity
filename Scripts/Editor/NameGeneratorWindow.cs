1d337c481098b5dd6c0b9d099be5177314d742a9
Interkarma <interkarma@dfworkshop.net>
2016-04-18 14:28:11 +1000

    First pass at random name generation. Added NameGen.txt file which is an export of name parts from NAMEGEN.DAT. Implemented NameHelper to supply name generation logic as known so far. Added NameGeneratorWindow to test name generation as it develops.

---
+++ Scripts/Editor/NameGeneratorWindow.cs
@@ -0,0 +1,100 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2015 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using DaggerfallWorkshop.Utility;
+using DaggerfallWorkshop.Game.Player;
+using DaggerfallWorkshop.Game.Items;
+using DaggerfallConnect;
+using DaggerfallConnect.Arena2;
+using DaggerfallConnect.Save;
+using DaggerfallConnect.Utility;
+using DaggerfallConnect.FallExe;
+using DaggerfallWorkshop.Game.Utility;
+using DaggerfallWorkshop.Game.Entity;
+
+namespace DaggerfallWorkshop
+{
+    /// <summary>
+    /// Test name generator in Unity editor.
+    /// </summary>
+    public class NameGeneratorWindow : EditorWindow
+    {
+        const string windowTitle = "Name Generator";
+        const string menuPath = "Daggerfall Tools/Name Generator [Beta]";
+
+        DaggerfallUnity dfUnity;
+        NameHelper nameHelper;
+
+        NameHelper.BankTypes bankType = NameHelper.BankTypes.Breton;
+        Genders gender = Genders.Male;
+        int count = 20;
+        List<string> generatedNames = new List<string>();
+        Vector2 scrollPos = Vector2.zero;
+
+        [MenuItem(menuPath)]
+        static void Init()
+        {
+            NameGeneratorWindow window = (NameGeneratorWindow)EditorWindow.GetWindow(typeof(NameGeneratorWindow));
+#if UNITY_5_0
+            window.title = windowTitle;
+#elif UNITY_5_1 || UNITY_5_2 || UNITY_5_3
+            window.titleContent = new GUIContent(windowTitle);
+#endif
+        }
+
+        void OnGUI()
+        {
+            if (!IsReady())
+            {
+                EditorGUILayout.HelpBox("DaggerfallUnity instance not ready. Have you set your Arena2 path?", MessageType.Info);
+                return;
+            }
+
+            EditorGUILayout.Space();
+            bankType = (NameHelper.BankTypes)EditorGUILayout.EnumPopup(new GUIContent("Type"), bankType);
+            gender = (Genders)EditorGUILayout.EnumPopup(new GUIContent("Gender"), gender);
+            count = EditorGUILayout.IntField(new GUIContent("Count"), count);
+            if (GUILayout.Button("Generate Names"))
+            {
+                generatedNames.Clear();
+                for (int i = 0; i < count; i++)
+                {
+                    generatedNames.Add(nameHelper.FullName(bankType, gender));
+                }
+            }
+
+            scrollPos = GUILayoutHelper.ScrollView(scrollPos, () =>
+            {
+                for (int i = 0; i < generatedNames.Count; i++)
+                {
+                    EditorGUILayout.SelectableLabel(generatedNames[i], EditorStyles.textField, GUILayout.Height(EditorGUIUtility.singleLineHeight));
+                }
+            });
+        }
+
+        bool IsReady()
+        {
+            if (!dfUnity)
+                dfUnity = DaggerfallUnity.Instance;
+
+            if (nameHelper == null)
+                nameHelper = new NameHelper();
+
+            return true;
+        }
+    }
+}
