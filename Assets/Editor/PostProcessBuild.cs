e06bace9a9580cd397ae59661af65b378fdcfab6
Interkarma <interkarma@dfworkshop.net>
2016-07-17 20:37:12 +1000

    Added new Editor folder and PostProcessBuild

---
+++ Assets/Editor/PostProcessBuild.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+
+public class PostProcessBuild
+{
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64 ||
+            target == BuildTarget.StandaloneLinux || target == BuildTarget.StandaloneLinux64 || target == BuildTarget.StandaloneLinuxUniversal ||
+            target == BuildTarget.StandaloneOSXIntel || target == BuildTarget.StandaloneOSXIntel64 || target == BuildTarget.StandaloneOSXUniversal)
+        {
+            // Remove PDB files
+            string pureBuildPath = Path.GetDirectoryName(pathToBuiltProject);
+            foreach (string file in Directory.GetFiles(pureBuildPath, "*.pdb"))
+            {
+                Debug.Log(file + " deleted!");
+                File.Delete(file);
+            }
+        }
+    }
+}
