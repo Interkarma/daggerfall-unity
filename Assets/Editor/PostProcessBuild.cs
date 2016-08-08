fd9d62c005c5f48556aa32381bbe500e9d70eea4
Interkarma <interkarma@dfworkshop.net>
2016-08-08 10:09:54 +1000

    Updated post build process
    Now creates default StreamingAssets folder for mods.

--- Assets/Editor/PostProcessBuild.cs
+++ Assets/Editor/PostProcessBuild.cs
@@ -8,17 +8,24 @@ public class PostProcessBuild
     [PostProcessBuildAttribute(1)]
     public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
     {
+        const string defaultModsFolderName = "StreamingAssets";
+
         if (target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64 ||
             target == BuildTarget.StandaloneLinux || target == BuildTarget.StandaloneLinux64 || target == BuildTarget.StandaloneLinuxUniversal ||
             target == BuildTarget.StandaloneOSXIntel || target == BuildTarget.StandaloneOSXIntel64 || target == BuildTarget.StandaloneOSXUniversal)
         {
-            // Remove PDB files
+            // Get build path
             string pureBuildPath = Path.GetDirectoryName(pathToBuiltProject);
+
+            // Remove PDB files
             foreach (string file in Directory.GetFiles(pureBuildPath, "*.pdb"))
             {
                 Debug.Log(file + " deleted!");
                 File.Delete(file);
             }
+
+            // Create default mods folder
+            Directory.CreateDirectory(Path.Combine(pureBuildPath, defaultModsFolderName));
         }
     }
 }
