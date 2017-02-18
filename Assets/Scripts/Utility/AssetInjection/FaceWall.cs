b90d3631493da58ca289ca536eb5c47531e9ee43
TheLacus <TheLacus@users.noreply.github.com>
2017-02-18 14:16:41 +0100

    Mesh and Texture replacement - More on FaceWall

--- Assets/Scripts/Utility/AssetInjection/FaceWall.cs
+++ Assets/Scripts/Utility/AssetInjection/FaceWall.cs
@@ -22,36 +22,66 @@ public class FaceWall : MonoBehaviour
 {
     // Max distance between the GameObject and the wall. 
     // A big value can cause wrong results.
-    float maxDistance = 0.6f;
+    public float maxDistance = 0.6f;
 
-    // Initialization
+    // Translate the GameObject so that it touches the wall
+    public bool AlignToWall = true;
+
+    /// <summary>
+    /// Initialization
+    /// </summary>
     void Start ()
     {
-        // Get GameObject Transform
-        Transform transform = gameObject.transform;
-
         // Get current position and rotate accordingly
-        if (HitWall(Vector3.forward))
-            return;
-        else if (HitWall(Vector3.left))
-            transform.Rotate(Vector3.up, -90f);
-        else if (HitWall(Vector3.right))
-            transform.Rotate(Vector3.up, 90f);
-        else if (HitWall(Vector3.back))
-            transform.Rotate(Vector3.up, 180f);
+        if (!HitWall(Vector3.forward))
+        {
+            if (HitWall(Vector3.left))
+                transform.Rotate(Vector3.up, -90f);
+            else if (HitWall(Vector3.right))
+                transform.Rotate(Vector3.up, 90f);
+            else if (HitWall(Vector3.back))
+                transform.Rotate(Vector3.up, 180f);
+        }
+
+        // Align to Wall
+        if (AlignToWall)
+            AlignGameObjectToWall();
     }
 
-    // Cast ray along direction to find the wall
-    bool HitWall (Vector3 direction)
+    /// <summary>
+    /// Cast ray along direction to find the wall
+    /// </summary>
+    /// <param name="direction">Direction to look for the wall</param>
+    /// <returns>True if wall is found</returns>
+    private bool HitWall (Vector3 direction)
     {
         // Transforms direction from local space to world space.
         direction = transform.TransformDirection(direction);
 
         // Casts a ray from the GameObject and check if it intersect 
         // with a collider in the specified direction.
-        if (Physics.Raycast(gameObject.transform.position, direction, maxDistance))
-            return true;
-        else
-            return false;
+        return Physics.Raycast(gameObject.transform.position, direction, maxDistance);
+    }
+
+    /// <summary>
+    /// Translate the GameObject so that it touches the wall
+    /// </summary>
+    private void AlignGameObjectToWall()
+    {
+        // Find point on wall
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+        if (!Physics.Raycast(ray, out hitInfo, maxDistance))
+            return;
+
+        // Find nearest point on collider
+        Vector3 pointOnGameObject = GetComponent<Collider>().ClosestPointOnBounds(hitInfo.point);
+        pointOnGameObject.y = transform.position.y;
+
+        // Get distance
+        float distance = Vector3.Distance(pointOnGameObject, hitInfo.point);
+
+        // Translate GameObject near wall
+        transform.Translate(0, 0, distance);
     }
 }
