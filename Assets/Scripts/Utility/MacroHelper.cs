3477899d1d7d177cc493fdef3d2289414dfa605e
Gavin Clayton <interkarma@dfworkshop.net>
2017-03-17 09:20:06 +1000

    Improvements to MacroHelper
    New GetMacro() method attempts to isolate actual macro token inside
    source word and provide more useful information such as actual macro
    token and position of macro token inside string regardless of adjacent
    characters like fullstop.
    Macro expansion now replaces just the token part rather than entire word
    so adjacent characters like fullstop are not replaced also.

--- Assets/Scripts/Utility/MacroHelper.cs
+++ Assets/Scripts/Utility/MacroHelper.cs
@@ -34,8 +34,11 @@ namespace DaggerfallWorkshop.Utility
         /// </summary>
         struct Macro
         {
-            public MacroTypes type;
-            public string symbol;
+            public string token;            // String token of any macro found (e.g. "__symbol_")
+            public MacroTypes type;         // Type of macro found (MacroTypes.None if not found)
+            public string symbol;           // Inner symbol of macro
+            public int index;               // Index of first macro character
+            public int length;              // Length of macro text from index
         }
 
         #endregion
@@ -61,7 +64,7 @@ namespace DaggerfallWorkshop.Utility
                 // Iterate words to find macros
                 for (int word = 0; word < words.Length; word++)
                 {
-                    Macro macro = MatchMacro(words[word]);
+                    Macro macro = GetMacro(words[word]);
                     if (macro.type == MacroTypes.ContextMacro)
                     {
                         // TODO: Get a quest context macro like %qdt
@@ -75,7 +78,7 @@ namespace DaggerfallWorkshop.Utility
                             string result;
                             if (resource.ExpandMacro(macro.type, out result))
                             {
-                                words[word] = result;
+                                words[word] = words[word].Replace(macro.token, result);
                             }
                         }
                     }
@@ -111,46 +114,101 @@ namespace DaggerfallWorkshop.Utility
 
 
         /// <summary>
-        /// Gets a macro from source word.
+        /// Attempts to get macro data from a word string.
+        /// Only a single macro will be matched per word.
         /// </summary>
-        /// <param name="word">Word to check.</param>
-        /// <returns>Macro. Type will be be None if word not matching any macros.</returns>
-        Macro MatchMacro(string word)
+        /// <param name="word">Source word to inspect for macro.</param>
+        /// <returns>Macro data, if a macro is found. Type will be be MacroTypes.None if no macro found in text.</returns>
+        Macro GetMacro(string word)
         {
-            // Trim word in case macro at end of sentence
-            // Any other conflicting characters?
-            // Maybe change this method to use regex later
-            word = word.Trim('.');
+            string pattern = @"(?<prefix>____)(?<NameMacro4_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>___)(?<NameMacro3_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>__)(?<NameMacro2_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>_)(?<NameMacro1_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>==)(?<FactionMacro_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>=#)(?<BindingMacro_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>=)(?<DetailsMacro_Symbol>[a-zA-Z0-9.]+)(?<suffix>_)|" +
+                             @"(?<prefix>%)(?<ContextMacro_Symbol>[a-zA-Z0-9.]+)";
 
-            // Get macro type
+            // Mactch macro type and inner symbol
             Macro macro = new Macro();
-            if (word.StartsWith("%"))
+            Match match = Regex.Match(word, pattern);
+            if (match.Success)
             {
-                macro.type = MacroTypes.ContextMacro;
-            }
-            if (word.EndsWith("_"))
-            {
-                if (word.StartsWith("____"))
-                    macro.type = MacroTypes.NameMacro4;
-                else if (word.StartsWith("___"))
-                    macro.type = MacroTypes.NameMacro3;
-                else if (word.StartsWith("__"))
-                    macro.type = MacroTypes.NameMacro2;
-                else if (word.StartsWith("_"))
-                    macro.type = MacroTypes.NameMacro1;
-                else if (word.StartsWith("=="))
-                    macro.type = MacroTypes.FactionMacro;
-                else if (word.StartsWith("=#"))
-                    macro.type = MacroTypes.BindingMacro;
-                else if (word.StartsWith("="))
-                    macro.type = MacroTypes.DetailsMacro;
-                else
-                    macro.type = MacroTypes.None;
-            }
+                // Get possible groups from pattern to isolate match value
+                Group foundGroup = null;
+                Group NameMacro4_Group = match.Groups["NameMacro4_Symbol"];
+                Group NameMacro3_Group = match.Groups["NameMacro3_Symbol"];
+                Group NameMacro2_Group = match.Groups["NameMacro2_Symbol"];
+                Group NameMacro1_Group = match.Groups["NameMacro1_Symbol"];
+                Group FactionMacro_Group = match.Groups["FactionMacro_Symbol"];
+                Group BindingMacro_Group = match.Groups["BindingMacro_Symbol"];
+                Group DetailsMacro_Group = match.Groups["DetailsMacro_Symbol"];
+                Group ContextMacro_Group = match.Groups["ContextMacro_Symbol"];
 
-            // Assign symbol
-            if (macro.type != MacroTypes.None)
-                macro.symbol = Parser.GetInnerSymbolName(word);
+                // Check which match group (if any) was found
+                if (!string.IsNullOrEmpty(NameMacro4_Group.Value))
+                {
+                    macro.type = MacroTypes.NameMacro4;
+                    foundGroup = NameMacro4_Group;
+                }
+                else if (!string.IsNullOrEmpty(NameMacro3_Group.Value))
+                {
+                    macro.type = MacroTypes.NameMacro3;
+                    foundGroup = NameMacro3_Group;
+                }
+                else if (!string.IsNullOrEmpty(NameMacro2_Group.Value))
+                {
+                    macro.type = MacroTypes.NameMacro2;
+                    foundGroup = NameMacro2_Group;
+                }
+                else if (!string.IsNullOrEmpty(NameMacro1_Group.Value))
+                {
+                    macro.type = MacroTypes.NameMacro1;
+                    foundGroup = NameMacro1_Group;
+                }
+                else if (!string.IsNullOrEmpty(FactionMacro_Group.Value))
+                {
+                    macro.type = MacroTypes.FactionMacro;
+                    foundGroup = FactionMacro_Group;
+                }
+                else if (!string.IsNullOrEmpty(BindingMacro_Group.Value))
+                {
+                    macro.type = MacroTypes.BindingMacro;
+                    foundGroup = BindingMacro_Group;
+                }
+                else if (!string.IsNullOrEmpty(DetailsMacro_Group.Value))
+                {
+                    macro.type = MacroTypes.DetailsMacro;
+                    foundGroup = DetailsMacro_Group;
+                }
+                else if (!string.IsNullOrEmpty(ContextMacro_Group.Value))
+                {
+                    macro.type = MacroTypes.ContextMacro;
+                    foundGroup = ContextMacro_Group;
+                }
+
+                // Set macro data if found
+                if (foundGroup != null)
+                {
+                    Group prefix = match.Groups["prefix"];
+                    Group suffix = match.Groups["suffix"];
+
+                    macro.symbol = foundGroup.Value;
+                    macro.index = prefix.Index;
+
+                    // Length is from first character to end of suffix (if present)
+                    // This will exclude any other characters (like fullstop) adjacent to macro token in word
+                    if (macro.type != MacroTypes.ContextMacro)
+                        macro.length = suffix.Index + suffix.Length - macro.index;
+                    else
+                        macro.length = prefix.Length + macro.symbol.Length;
+
+                    // Get substring of macro token alone
+                    // This is used for replace later
+                    macro.token = word.Substring(macro.index, macro.length);
+                }
+            }
 
             return macro;
         }
