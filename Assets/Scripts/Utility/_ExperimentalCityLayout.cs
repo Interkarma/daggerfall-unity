d056035889dae1ef2c48e8b7dddc7b2f60b03ce7
Gavin Clayton <interkarma@dfworkshop.net>
2017-03-19 08:44:10 +1000

    Experimental layout
    Adding a few comments and more display data to strings.

--- Assets/Scripts/Utility/_ExperimentalCityLayout.cs
+++ Assets/Scripts/Utility/_ExperimentalCityLayout.cs
@@ -52,23 +52,28 @@ namespace DaggerfallWorkshop.Utility
             int height = location.Exterior.ExteriorData.Height;
 
             // Import buildings
-            AddBuildings(ref location, locationGameObject);
+            int totalMapBuildings = AddBuildings(ref location, locationGameObject);
 
             // Import blocks
+            int totalBlockBuildings = 0;
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    //AddBlock(ref location, x, y, locationGameObject);
+                    totalBlockBuildings += AddBlock(ref location, x, y, locationGameObject);
                 }
             }
+
+            Debug.LogFormat("Total map buildings: {0}, Total block buildings: {1}", totalMapBuildings, totalBlockBuildings);
         }
 
         /// <summary>
         /// One problem I'm still not satisfied with is how to correctly link buildings in map data to buildings in block data.
         /// </summary>
-        void AddBuildings(ref DFLocation location, GameObject parent)
+        int AddBuildings(ref DFLocation location, GameObject parent)
         {
+            int totalMapBuildings = 0;
+
             // Location buildings
             int buildingCount = location.Exterior.BuildingCount;
             GameObject[] buildingGameObjectArray = new GameObject[buildingCount];
@@ -79,28 +84,37 @@ namespace DaggerfallWorkshop.Utility
 
                 string buildingName = BuildingNames.GetName(building.NameSeed, building.BuildingType, building.FactionId, location.Name, location.RegionName);
 
-                buildingGameObjectArray[i] = new GameObject(string.Format("Building: {0} [{1}]", building.BuildingType, buildingName));
+                // Would love to know what this "Sector" value actually is
+                buildingGameObjectArray[i] = new GameObject(string.Format("Building: {0} [{1}]", building.BuildingType, building.Sector));
                 buildingGameObjectArray[i].transform.parent = parent.transform;
+                totalMapBuildings++;
             }
 
-            //// Location doors
-            //int doorCount = (int)location.Exterior.RecordElement.DoorCount;
-            //for (int i = 0; i < doorCount; i++)
-            //{
-            //    DFLocation.LocationDoorElement door = location.Exterior.RecordElement.Doors[i];
-            //    if (door.BuildingDataIndex == 0xffff)
-            //        continue;
+            // UESP guesses these records are doors. They do map to a building index but do not distribute as doors would.
+            // A building might have 0-N of these items regardless of how many doors that building actually has.
+            // What is this data, really?
+            // Observation: the Unknown1 value is often (but not always) +2-3 greater than Sector value on building itself.
+            // Observation: templates often have a larger number of these records attached than any other building type.
+            int doorCount = (int)location.Exterior.RecordElement.DoorCount;
+            for (int i = 0; i < doorCount; i++)
+            {
+                DFLocation.LocationDoorElement door = location.Exterior.RecordElement.Doors[i];
+                if (door.BuildingDataIndex == 0xffff)
+                    continue;
 
-            //    cityDoors.Add(door);
+                cityDoors.Add(door);
+                GameObject doorGameObject = new GameObject(string.Format("Door: Mask={0}, Unknown1={1}, Unknown2={2}", door.Mask, door.Unknown1, door.Unknown2));
+                doorGameObject.transform.parent = buildingGameObjectArray[door.BuildingDataIndex].transform;
+            }
 
-            //    GameObject doorGameObject = new GameObject(string.Format("Door: {0}", door.Unknown1));
-            //    doorGameObject.transform.parent = buildingGameObjectArray[door.BuildingDataIndex].transform;
-            //}
+            return totalMapBuildings;
         }
 
         
-        void AddBlock(ref DFLocation location, int x, int y, GameObject parent)
+        int AddBlock(ref DFLocation location, int x, int y, GameObject parent)
         {
+            int totalBlockBuildings = 0;
+
             // Get block name
             string blockName = contentReader.BlockFileReader.CheckName(contentReader.MapFileReader.GetRmbBlockName(ref location, x, y));
 
@@ -115,13 +129,26 @@ namespace DaggerfallWorkshop.Utility
             blockGameObject.transform.position = new Vector3((x * RMBLayout.RMBSide), 0, (y * RMBLayout.RMBSide));
 
             int buildingCount = block.RmbBlock.SubRecords.Length;
-            for (int i = 0; i < buildingCount; i++)
+            for (int i = 0; i < 32; i++)
             {
-                //DFBlock.RmbBlockHeader header = block.RmbBlock.SubRecords[i].Exterior.Header;
                 DFLocation.BuildingData building = block.RmbBlock.FldHeader.BuildingDataList[i];
-                GameObject buildingGameObject = new GameObject(string.Format("Building: {0}", building.BuildingType));
+
+                string buildingName;
+                if (i < buildingCount)
+                {
+                    buildingName = string.Format("[{0}] Building: {1}", i, building.BuildingType);
+                    totalBlockBuildings++;
+                }
+                else
+                {
+                    buildingName = string.Format("[{0}] Unused", i);
+                }
+                //DFBlock.RmbBlockHeader header = block.RmbBlock.SubRecords[i].Exterior.Header;
+                GameObject buildingGameObject = new GameObject(buildingName);
                 buildingGameObject.transform.parent = blockGameObject.transform;
             }
+
+            return totalBlockBuildings;
         }
     }
 }
