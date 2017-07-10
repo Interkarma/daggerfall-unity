8b8e490fc22c5788b9e99dc27303c79edf59b591
Gavin Clayton <interkarma@dfworkshop.net>
2017-07-10 15:59:00 +1000

    Implemented dungeon multi-spawn
    Using a raycast system to find a nearby spawn point outside of player's
    field of view.
    Enemies can now be pending spawn until the next available spawn point is
    found. In tight spaces, this has the effect of dropping enemies as
    player turns a corner or spins around, or passes an open door into
    another space. Despite working differently under the hood, the final
    effect feels very similar to classic.
    Added __DEMO13 to test harpy waves in Privateer's Hold.

--- Assets/Scripts/Game/Questing/Actions/CreateFoe.cs
+++ Assets/Scripts/Game/Questing/Actions/CreateFoe.cs
@@ -31,6 +31,10 @@ namespace DaggerfallWorkshop.Game.Questing
         ulong lastSpawnTime = 0;
         int spawnCounter = 0;
 
+        bool spawnInProgress = false;
+        GameObject[] pendingFoeGameObjects;
+        int pendingFoesSpawned;
+
         public override string Pattern
         {
             get
@@ -81,48 +85,78 @@ namespace DaggerfallWorkshop.Game.Questing
             if (spawnCounter >= spawnMaxTimes && spawnMaxTimes != -1)
                 return;
 
-            // Check for a spawn event
-            ulong gameSeconds = DaggerfallUnity.Instance.WorldTime.DaggerfallDateTime.ToSeconds();
-            if (gameSeconds > lastSpawnTime + spawnInterval)
+            // Clear pending foes if all have been spawned
+            if (spawnInProgress && pendingFoesSpawned >= pendingFoeGameObjects.Length)
             {
-                // Spawn the foe resource
-                SpawnFoe();
+                spawnInProgress = false;
+                spawnCounter++;
+                return;
+            }
 
-                // Update last spawn time and timer
+            // Check for a new spawn event - only one spawn event can be running at a time
+            ulong gameSeconds = DaggerfallUnity.Instance.WorldTime.DaggerfallDateTime.ToSeconds();
+            if (gameSeconds > lastSpawnTime + spawnInterval && !spawnInProgress)
+            {
+                // Update last spawn time
                 lastSpawnTime = gameSeconds;
+
+                // Roll for spawn chance
+                float chance = spawnChance / 100f;
+                if (UnityEngine.Random.Range(0f, 1f) > chance)
+                    return;
+
+                // Start deploying GameObjects
+                CreatePendingFoeSpawn();
+            }
+
+            // Try to deploy a pending spawns
+            if (spawnInProgress)
+            {
+                TryPlacement();
             }
         }
 
-        void SpawnFoe()
-        {
-            // Roll for spawn chance
-            float chance = spawnChance / 100f;
-            if (UnityEngine.Random.Range(0f, 1f) > chance)
-                return;
+        #region Private Methods
 
+        void CreatePendingFoeSpawn()
+        {
             // Get the Foe resource
             Foe foe = ParentQuest.GetFoe(foeSymbol);
             if (foe == null)
                 throw new Exception(string.Format("create foe could not find Foe with symbol name {0}", Symbol.Name));
 
-            // Get foe objects
-            GameObject[] gameObjects = foe.CreateFoeGameObjects(Vector3.zero);
-            if (gameObjects == null || gameObjects.Length != foe.SpawnCount)
-                throw new Exception(string.Format("create foe attempted to spawn {0}x{1} and failed.", foe.SpawnCount, Symbol.Name));
+            // Get foe GameObjects
+            pendingFoeGameObjects = foe.CreateFoeGameObjects(Vector3.zero);
+            if (pendingFoeGameObjects == null || pendingFoeGameObjects.Length != foe.SpawnCount)
+                throw new Exception(string.Format("create foe attempted to create {0}x{1} GameObjects and failed.", foe.SpawnCount, Symbol.Name));
 
+            // Initiate deployment process
+            // Usually the foe will spawn immediately but can take longer depending on available placement space
+            // This process ensures these foes have all been deployed before starting next cycle
+            spawnInProgress = true;
+            pendingFoesSpawned = 0;
+        }
+
+        void TryPlacement()
+        {
             // Place in world near player depending on local area
             PlayerEnterExit playerEnterExit = GameManager.Instance.PlayerEnterExit;
             if (playerEnterExit.IsPlayerInsideBuilding)
             {
-                PlaceFoeBuildingInterior(gameObjects, playerEnterExit.Interior);
+                PlaceFoeBuildingInterior(pendingFoeGameObjects, playerEnterExit.Interior);
             }
-
-            // Increment counter
-            spawnCounter++;
+            else if (playerEnterExit.IsPlayerInsideDungeon)
+            {
+                PlaceFoeDungeonInterior(pendingFoeGameObjects, playerEnterExit.Dungeon);
+            }
         }
 
+        #endregion
+
+        #region Placement Methods
+
         // Place foe somewhere near player when inside a building
-        // Building interiors have spawn nodes for this placement
+        // Building interiors have spawn nodes for this placement so we can roll out foes all at once
         void PlaceFoeBuildingInterior(GameObject[] gameObjects, DaggerfallInterior interiorParent)
         {
             foreach(GameObject go in gameObjects)
@@ -130,22 +164,71 @@ namespace DaggerfallWorkshop.Game.Questing
                 Vector3 spawnPosition;
                 bool spawnPositionFound = interiorParent.GetRandomSpawnPoint(out spawnPosition);
                 AlignFoe(go, interiorParent.transform, spawnPosition, !spawnPositionFound);
+                pendingFoesSpawned++;
             }
-        }
-
-        // Place foe somewhere near player when outside a location navgrid is available
-        // Navgrid placement helps foe avoid getting tangled in geometry like buildings
-        void PlaceFoeExteriorLocation(GameObject[] gameObjects)
-        {
-            Debug.LogFormat("Attempt was made to place {0} foes to Exterior Location, but this has not been implemented yet.", gameObjects.Length);
         }
 
         // Place foe somewhere near player when inside a dungeon
         // Dungeons interiors are complex 3D environments with no navgrid/navmesh or (known) spawn nodes
-        // This solution will use the nearest "random flat" editor marker for now
-        void PlaceFoeDungeonInterior(GameObject[] gameObjects)
+        // This solution uses raycasts to find a spawn position
+        void PlaceFoeDungeonInterior(GameObject[] gameObjects, DaggerfallDungeon dungeonParent)
         {
-            Debug.LogFormat("Attempt was made to place {0} foes to Dungeon Interior, but this has not been implemented yet.", gameObjects.Length);
+            const float overlapSphereRadius = 0.45f;
+            const float separationDistance = 1.25f;
+            const float maxFloorDistance = 4f;
+            const float minDistance = 4f;
+            const float maxDistance = 20f;
+
+            // Select a left or right direction outside of camera FOV
+            Quaternion rotation;
+            float directionAngle = GameManager.Instance.MainCamera.fieldOfView;
+            if (UnityEngine.Random.Range(0f, 1f) > 1000.5f)
+                rotation = Quaternion.Euler(0, -directionAngle, 0);
+            else
+                rotation = Quaternion.Euler(0, directionAngle, 0);
+
+            // Get direction vector and create a new ray
+            Vector3 angle = (rotation * Vector3.forward).normalized;
+            Vector3 spawnDirection = GameManager.Instance.PlayerObject.transform.TransformDirection(angle);
+            Ray ray = new Ray(GameManager.Instance.PlayerObject.transform.position, spawnDirection);
+
+            // Check for a hit
+            RaycastHit initialHit;
+            if (!Physics.Raycast(ray, out initialHit, maxDistance))
+                return;
+
+            // Must be greater than minDistance
+            if (initialHit.distance < minDistance)
+                return;
+
+            // Separate out from hit point
+            Vector3 currentPoint = initialHit.point + initialHit.normal.normalized * separationDistance;
+
+            // Must be able to find a surface below
+            RaycastHit floorHit;
+            ray = new Ray(currentPoint, Vector3.down);
+            if (!Physics.Raycast(ray, out floorHit, maxFloorDistance))
+                return;
+
+            // Ensure this is open space
+            Vector3 testPoint = floorHit.point + Vector3.up * separationDistance;
+            Collider[] colliders = Physics.OverlapSphere(testPoint, overlapSphereRadius);
+            if (colliders.Length > 0)
+                return;
+
+            // This looks like a good spawn position
+            AlignFoe(pendingFoeGameObjects[pendingFoesSpawned], dungeonParent.transform, testPoint);
+            gameObjects[pendingFoesSpawned].transform.LookAt(GameManager.Instance.PlayerObject.transform.position);
+
+            // Increment count
+            pendingFoesSpawned++;
+        }
+
+        // Place foe somewhere near player when outside a location navgrid is available
+        // Navgrid placement helps foe avoid getting tangled in geometry like buildings
+        void PlaceFoeExteriorLocation(GameObject[] gameObjects, DaggerfallLocation locationParent)
+        {
+            Debug.LogFormat("Attempt was made to place {0} foes to Exterior Location, but this has not been implemented yet.", gameObjects.Length);
         }
 
         // Place foe somewhere near player when outside and no navgrid available
@@ -189,5 +272,7 @@ namespace DaggerfallWorkshop.Game.Questing
 
             go.SetActive(true);
         }
+
+        #endregion
     }
 }
