13620b7995d62b58d071ea5aa7b50fc72b38a3ad
Nystul-the-Magician <11569250+Nystul-the-Magician@users.noreply.github.com>
2017-09-04 16:19:32 +0200

    talk mode and talk category selection implemented

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallTalkWindow.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallTalkWindow.cs
@@ -15,17 +15,68 @@ using System.Collections;
 using DaggerfallWorkshop.Game.UserInterface;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
 
+using DaggerfallConnect;
+using DaggerfallConnect.Arena2;
+using DaggerfallConnect.Utility;
+using DaggerfallWorkshop;
+using DaggerfallWorkshop.Utility;
+
+using System.IO;
+using System.Collections.Generic;
+
 namespace DaggerfallWorkshop.Game.UserInterface
 {
     public class DaggerfallTalkWindow : DaggerfallPopupWindow
     {
-        const string IMGNAME    = "TALK01I0.IMG";
+        const string talkWindowImgName    = "TALK01I0.IMG";
+        const string talkCategoriesImgName = "TALK02I0.IMG";
+        const string highlightedOptionsImgName = "TALK03I0.IMG";
+
+        enum SelectedTalkOption { 
+            TellMeAbout,
+            WhereIs
+        };
+        SelectedTalkOption selectedTalkOption = SelectedTalkOption.TellMeAbout;
+
+        enum SelectedTalkCategory
+        {
+            None,
+            Location,
+            People,
+            Things,
+            Work
+        };
+        SelectedTalkCategory selectedTalkCategory = SelectedTalkCategory.Location;
+
+        Texture2D textureBackground;
+        Texture2D textureHighlightedOptions;
+        Texture2D textureGrayedOutCategories;
+
+        Color[] textureTellMeAboutNormal;
+        Color[] textureTellMeAboutHighlighted;
+        Color[] textureWhereIsNormal;
+        Color[] textureWhereIsHighlighted;
+
+        Color[] textureCategoryLocationHighlighted;
+        Color[] textureCategoryLocationGrayedOut;
+        Color[] textureCategoryPeopleHighlighted;
+        Color[] textureCategoryPeopleGrayedOut;
+        Color[] textureCategoryThingsHighlighted;
+        Color[] textureCategoryThingsGrayedOut;
+        Color[] textureCategoryWorkHighlighted;
+        Color[] textureCategoryWorkGrayedOut;
 
         Panel mainPanel;
 
         TextLabel pcSay;
 
-        Button goodbyeButton;
+        Button buttonTellMeAbout;
+        Button buttonWhereIs;
+        Button buttonCategoryLocation;
+        Button buttonCategoryPeople;
+        Button buttonCategoryThings;
+        Button buttonCategoryWork;
+        Button buttonGoodbye;
 
         public DaggerfallTalkWindow(IUserInterfaceManager uiManager, DaggerfallBaseWindow previous = null)
             : base(uiManager, previous)
@@ -36,19 +87,73 @@ namespace DaggerfallWorkshop.Game.UserInterface
         {
             base.Setup();
 
-            var background = DaggerfallUI.GetTextureFromImg(IMGNAME);
-            if (background == null)
+            ImgFile imgFile = null;
+            DFBitmap bitmap = null;
+
+            // Load background texture of talk window
+            imgFile = new ImgFile(Path.Combine(DaggerfallUnity.Instance.Arena2Path, talkWindowImgName), FileUsage.UseMemory, false);
+            imgFile.LoadPalette(Path.Combine(DaggerfallUnity.Instance.Arena2Path, imgFile.PaletteName));
+            bitmap = imgFile.GetDFBitmap();
+            textureBackground = new Texture2D(bitmap.Width, bitmap.Height, TextureFormat.ARGB32, false);
+            textureBackground.SetPixels32(imgFile.GetColor32(bitmap, 0));
+            textureBackground.Apply(false, false); // make readable
+            textureBackground.filterMode = DaggerfallUI.Instance.GlobalFilterMode;
+            if (!textureBackground)
             {
-                Debug.LogError(string.Format("Failed to load background image {0} for talk window", IMGNAME));
+                Debug.LogError(string.Format("Failed to load background image {0} for talk window", talkWindowImgName));
                 CloseWindow();
                 return;
             }
 
+            // Load talk options highlight texture
+            imgFile = new ImgFile(Path.Combine(DaggerfallUnity.Instance.Arena2Path, highlightedOptionsImgName), FileUsage.UseMemory, false);
+            imgFile.LoadPalette(Path.Combine(DaggerfallUnity.Instance.Arena2Path, imgFile.PaletteName));
+            bitmap = imgFile.GetDFBitmap();
+            textureHighlightedOptions = new Texture2D(bitmap.Width, bitmap.Height, TextureFormat.ARGB32, false);
+            textureHighlightedOptions.SetPixels32(imgFile.GetColor32(bitmap, 0));
+            textureHighlightedOptions.Apply(false, false); // make readable
+            textureHighlightedOptions.filterMode = DaggerfallUI.Instance.GlobalFilterMode;
+            if (!textureHighlightedOptions)
+            {
+                Debug.LogError(string.Format("Failed to load highlighted options image {0} for talk window", highlightedOptionsImgName));
+                CloseWindow();
+                return;
+            }
+
+            // Load talk categories highlight texture
+            imgFile = new ImgFile(Path.Combine(DaggerfallUnity.Instance.Arena2Path, talkCategoriesImgName), FileUsage.UseMemory, false);
+            imgFile.LoadPalette(Path.Combine(DaggerfallUnity.Instance.Arena2Path, imgFile.PaletteName));
+            bitmap = imgFile.GetDFBitmap();
+            textureGrayedOutCategories = new Texture2D(bitmap.Width, bitmap.Height, TextureFormat.ARGB32, false);
+            textureGrayedOutCategories.SetPixels32(imgFile.GetColor32(bitmap, 0));
+            textureGrayedOutCategories.Apply(false, false); // make readable
+            textureGrayedOutCategories.filterMode = DaggerfallUI.Instance.GlobalFilterMode;
+            if (!textureGrayedOutCategories)
+            {
+                Debug.LogError(string.Format("Failed to load grayed-out categories image {0} for talk window", textureGrayedOutCategories));
+                CloseWindow();
+                return;
+            }
+
+            textureTellMeAboutNormal = textureBackground.GetPixels(4, textureBackground.height - 4 - 10, 107, 10);
+            textureTellMeAboutHighlighted = textureHighlightedOptions.GetPixels(0, 10, 107, 10);
+            textureWhereIsNormal = textureBackground.GetPixels(4, textureBackground.height - 14 - 10, 107, 10);
+            textureWhereIsHighlighted = textureHighlightedOptions.GetPixels(0, 0, 107, 10);
+
+            textureCategoryLocationHighlighted = textureBackground.GetPixels(4, textureBackground.height - 26 - 10, 107, 10);
+            textureCategoryLocationGrayedOut = textureGrayedOutCategories.GetPixels(0, 30, 107, 10);
+            textureCategoryPeopleHighlighted = textureBackground.GetPixels(4, textureBackground.height - 36 - 10, 107, 10);
+            textureCategoryPeopleGrayedOut = textureGrayedOutCategories.GetPixels(0, 20, 107, 10);
+            textureCategoryThingsHighlighted = textureBackground.GetPixels(4, textureBackground.height - 46 - 10, 107, 10);
+            textureCategoryThingsGrayedOut = textureGrayedOutCategories.GetPixels(0, 10, 107, 10);
+            textureCategoryWorkHighlighted = textureBackground.GetPixels(4, textureBackground.height - 56 - 10, 107, 10);
+            textureCategoryWorkGrayedOut = textureGrayedOutCategories.GetPixels(0, 0, 107, 10);
+
             ParentPanel.BackgroundColor = ScreenDimColor;
 
             mainPanel                       = DaggerfallUI.AddPanel(NativePanel, AutoSizeModes.None);
-            mainPanel.BackgroundTexture     = background;
-            mainPanel.Size                  = new Vector2(background.width, background.height);
+            mainPanel.BackgroundTexture = textureBackground;
+            mainPanel.Size = new Vector2(textureBackground.width, textureBackground.height);
             mainPanel.HorizontalAlignment   = HorizontalAlignment.Center;
             mainPanel.VerticalAlignment     = VerticalAlignment.Middle;
 
@@ -61,14 +166,57 @@ namespace DaggerfallWorkshop.Game.UserInterface
             mainPanel.Components.Add(pcSay);
             */
 
-            goodbyeButton = new Button();
-            goodbyeButton.Position = new Vector2(118, 183);
-            goodbyeButton.Size = new Vector2(67, 10);
-            goodbyeButton.Name = "goodbye_button";
-            goodbyeButton.OnMouseClick += goodbyeButton_OnMouseClick;
-            mainPanel.Components.Add(goodbyeButton);
+            buttonTellMeAbout = new Button();
+            buttonTellMeAbout.Position = new Vector2(4, 4);
+            buttonTellMeAbout.Size = new Vector2(107, 10);
+            buttonTellMeAbout.Name = "button_tellmeabout";
+            buttonTellMeAbout.OnMouseClick += buttonTellMeAbout_OnMouseClick;
+            mainPanel.Components.Add(buttonTellMeAbout);
+
+            buttonWhereIs = new Button();
+            buttonWhereIs.Position = new Vector2(4, 14);
+            buttonWhereIs.Size = new Vector2(107, 10);
+            buttonWhereIs.Name = "button_whereis";
+            buttonWhereIs.OnMouseClick += buttonWhereIs_OnMouseClick;
+            mainPanel.Components.Add(buttonWhereIs);
+
+            buttonCategoryLocation = new Button();
+            buttonCategoryLocation.Position = new Vector2(4, 26);
+            buttonCategoryLocation.Size = new Vector2(107, 10);
+            buttonCategoryLocation.Name = "button_categoryLocation";
+            buttonCategoryLocation.OnMouseClick += buttonCategoryLocation_OnMouseClick;
+            mainPanel.Components.Add(buttonCategoryLocation);
+
+            buttonCategoryPeople = new Button();
+            buttonCategoryPeople.Position = new Vector2(4, 36);
+            buttonCategoryPeople.Size = new Vector2(107, 10);
+            buttonCategoryPeople.Name = "button_categoryPeople";
+            buttonCategoryPeople.OnMouseClick += buttonCategoryPeople_OnMouseClick;
+            mainPanel.Components.Add(buttonCategoryPeople);
+
+            buttonCategoryThings = new Button();
+            buttonCategoryThings.Position = new Vector2(4, 46);
+            buttonCategoryThings.Size = new Vector2(107, 10);
+            buttonCategoryThings.Name = "button_categoryThings";
+            buttonCategoryThings.OnMouseClick += buttonCategoryThings_OnMouseClick;
+            mainPanel.Components.Add(buttonCategoryThings);
+
+            buttonCategoryWork = new Button();
+            buttonCategoryWork.Position = new Vector2(4, 56);
+            buttonCategoryWork.Size = new Vector2(107, 10);
+            buttonCategoryWork.Name = "button_categoryWork";
+            buttonCategoryWork.OnMouseClick += buttonCategoryWork_OnMouseClick;
+            mainPanel.Components.Add(buttonCategoryWork);
+
+            buttonGoodbye = new Button();
+            buttonGoodbye.Position = new Vector2(118, 183);
+            buttonGoodbye.Size = new Vector2(67, 10);
+            buttonGoodbye.Name = "button_goodbye";
+            buttonGoodbye.OnMouseClick += buttonGoodbye_OnMouseClick;
+            mainPanel.Components.Add(buttonGoodbye);
 
             UpdateLabels();
+            UpdateButtons();
         }
 
         public override void OnPush()
@@ -94,14 +242,152 @@ namespace DaggerfallWorkshop.Game.UserInterface
 
         }
 
+        void setTalkModeTellMeAbout()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 4 - 10, 107, 10, textureTellMeAboutHighlighted);
+            textureBackground.SetPixels(4, textureBackground.height - 14 - 10, 107, 10, textureWhereIsNormal);
+            textureBackground.Apply(false);
+        }
+
+        void setTalkModeWhereIs()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 4 - 10, 107, 10, textureTellMeAboutNormal);
+            textureBackground.SetPixels(4, textureBackground.height - 14 - 10, 107, 10, textureWhereIsHighlighted);
+            textureBackground.Apply(false);
+        }
+
+        void setTalkCategoryNone()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 26 - 10, 107, 10, textureCategoryLocationGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 36 - 10, 107, 10, textureCategoryPeopleGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 46 - 10, 107, 10, textureCategoryThingsGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 56 - 10, 107, 10, textureCategoryWorkGrayedOut);
+            textureBackground.Apply(false);
+        }
+
+        void setTalkCategoryLocation()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 26 - 10, 107, 10, textureCategoryLocationHighlighted);
+            textureBackground.SetPixels(4, textureBackground.height - 36 - 10, 107, 10, textureCategoryPeopleGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 46 - 10, 107, 10, textureCategoryThingsGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 56 - 10, 107, 10, textureCategoryWorkGrayedOut);
+            textureBackground.Apply(false);
+        }
+
+        void setTalkCategoryPeople()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 26 - 10, 107, 10, textureCategoryLocationGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 36 - 10, 107, 10, textureCategoryPeopleHighlighted);
+            textureBackground.SetPixels(4, textureBackground.height - 46 - 10, 107, 10, textureCategoryThingsGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 56 - 10, 107, 10, textureCategoryWorkGrayedOut);
+            textureBackground.Apply(false);
+        }
+
+        void setTalkCategoryThings()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 26 - 10, 107, 10, textureCategoryLocationGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 36 - 10, 107, 10, textureCategoryPeopleGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 46 - 10, 107, 10, textureCategoryThingsHighlighted);
+            textureBackground.SetPixels(4, textureBackground.height - 56 - 10, 107, 10, textureCategoryWorkGrayedOut);
+            textureBackground.Apply(false);
+        }
+
+        void setTalkCategoryWork()
+        {
+            textureBackground.SetPixels(4, textureBackground.height - 26 - 10, 107, 10, textureCategoryLocationGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 36 - 10, 107, 10, textureCategoryPeopleGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 46 - 10, 107, 10, textureCategoryThingsGrayedOut);
+            textureBackground.SetPixels(4, textureBackground.height - 56 - 10, 107, 10, textureCategoryWorkHighlighted);
+            textureBackground.Apply(false);
+        }
+
         void UpdateButtons()
         {
-
+            switch (selectedTalkOption)
+            {
+                case SelectedTalkOption.TellMeAbout:
+                    setTalkModeTellMeAbout();
+                    setTalkCategoryNone();
+                    break;
+                case SelectedTalkOption.WhereIs:
+                    setTalkModeWhereIs();
+                    switch (selectedTalkCategory)
+                    {
+                        case SelectedTalkCategory.Location:
+                            setTalkCategoryLocation();
+                            break;
+                        case SelectedTalkCategory.People:
+                            setTalkCategoryPeople();
+                            break;
+                        case SelectedTalkCategory.Things:
+                            setTalkCategoryThings();
+                            break;
+                        case SelectedTalkCategory.Work:
+                            setTalkCategoryWork();
+                            break;
+                        default:
+                            setTalkCategoryNone();
+                            break;
+                    }
+                    break;
+                default:
+                    setTalkModeTellMeAbout();
+                    setTalkCategoryNone();
+                    break;
+            }
         }
 
         #region event handlers
 
-        void goodbyeButton_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        void buttonTellMeAbout_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            selectedTalkOption = SelectedTalkOption.TellMeAbout;
+            UpdateButtons();
+        }
+
+        void buttonWhereIs_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            selectedTalkOption = SelectedTalkOption.WhereIs;
+            UpdateButtons();
+        }
+
+        void buttonCategoryLocation_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            if (selectedTalkOption == SelectedTalkOption.WhereIs)
+            {
+                selectedTalkCategory = SelectedTalkCategory.Location;
+                UpdateButtons();
+            }
+        }
+
+        void buttonCategoryPeople_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            if (selectedTalkOption == SelectedTalkOption.WhereIs)
+            {
+                selectedTalkCategory = SelectedTalkCategory.People;
+                UpdateButtons();
+            }
+        }
+
+        void buttonCategoryThings_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            if (selectedTalkOption == SelectedTalkOption.WhereIs)
+            {
+                selectedTalkCategory = SelectedTalkCategory.Things;
+                UpdateButtons();
+            }
+        }
+
+        void buttonCategoryWork_OnMouseClick(BaseScreenComponent sender, Vector2 position)
+        {
+            if (selectedTalkOption == SelectedTalkOption.WhereIs)
+            {
+                selectedTalkCategory = SelectedTalkCategory.Work;
+                UpdateButtons();
+            }
+        }       
+
+        void buttonGoodbye_OnMouseClick(BaseScreenComponent sender, Vector2 position)
         {
             CloseWindow();
         }
