8297c29a7bdd179fb4d432dbdeec3b4adfa3702d
Gavin Clayton <interkarma@dfworkshop.net>
2017-09-20 16:31:44 +1000

    Slightly lower origin of player activation ray
    This could previously intersect with player's own body capsule, blocking
    loot piles or low triggers (e.g. throne lever in Privateer's Hold).
    Lowering ray origin slightly makes it start inside player's own capsule
    and prevents a self-hit without using any layers.

--- Assets/Scripts/Game/PlayerActivate.cs
+++ Assets/Scripts/Game/PlayerActivate.cs
@@ -87,10 +87,9 @@ namespace DaggerfallWorkshop.Game
             {
                 // TODO: Clean all this up
 
-                // Was using RayCastAll here to avoid unresponsive feeling when objects block ray,
-                // but this did not allow for a long raycast, since it would find things through walls, etc.
-                // Also even with a short raycast, RayCastAll allowed accessing things through walls, etc.
-                Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+                // Ray origin is slightly below camera height to ensure it originates inside player's own collider
+                // This prevents ray from intersecting with player's own collider and blocking looting or low triggers
+                Ray ray = new Ray(transform.position + Vector3.up * 0.6f, mainCamera.transform.forward);
                 RaycastHit hit;
                 RayDistance = 75f; // Approximates classic at full view distance (default setting). Classic seems to do raycasts for as far as it can render objects.
                 bool hitSomething = Physics.Raycast(ray, out hit, RayDistance);
