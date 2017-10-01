079c945653372481791191c3c2fec98a13212c11
Gavin Clayton <interkarma@dfworkshop.net>
2017-10-01 15:13:05 +1000

    Fix mqstage 4 instantiation
    Fixed quest filename.
    Set static marker for wayrest painting.

--- Assets/Scripts/Game/Questing/QuestMachine.cs
+++ Assets/Scripts/Game/Questing/QuestMachine.cs
@@ -920,7 +920,7 @@ namespace DaggerfallWorkshop.Game.Questing
             {
                 GameManager.Instance.PlayerEntity.FactionData.ChangeReputation(380, 20);    // Queen Akorithi +20
                 GameManager.Instance.PlayerEntity.Level = 7;
-                InstantiateQuest("__MQSTAGE07");
+                InstantiateQuest("__MQSTAGE04");
                 InstantiateQuest("S0000977");
             }
 
