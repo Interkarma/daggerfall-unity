94ae47d2e8f2ddc34f599c986320b3d55dce677a
Allofich <rickywd@gmail.com>
2017-10-14 17:14:04 +0900

    Temporarily ignore player ship for travel costs

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallTravelPopUp.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallTravelPopUp.cs
@@ -246,7 +246,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             tripCost = travelTimeCalculator.CalculateTripCost(
                 travelTimeMinutes,
                 sleepModeInn,
-                hasShip,
+                false,  // TODO: Replace with hasShip when ship buying is implemented.
                 travelShip
                 );
 
