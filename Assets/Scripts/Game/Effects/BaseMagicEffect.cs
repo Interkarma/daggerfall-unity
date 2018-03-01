09f171037cc0ba85b59863b1ee6105717f904789
Gavin Clayton <interkarma@dfworkshop.net>
2018-03-01 16:20:57 +1000

    Rename SpellEffect to MagicEffect
    Spells are just one transport method for a magic effect. This might save
    confusion down the track.
    Start adding support for classic effect key.

--- Assets/Scripts/Game/Effects/BaseMagicEffect.cs
+++ Assets/Scripts/Game/Effects/BaseMagicEffect.cs
@@ -18,9 +18,9 @@ using DaggerfallConnect.Arena2;
 namespace DaggerfallWorkshop.Game.Effects
 {
     /// <summary>
-    /// Interface to a spell effect.
+    /// Interface to a magic effect.
     /// </summary>
-    public interface ISpellEffect
+    public interface IMagicEffect
     {
         /// <summary>
         /// Main group for this effect class in spellmaker (e.g. "Damage") - can be shared with other effects.
@@ -39,6 +39,11 @@ namespace DaggerfallWorkshop.Game.Effects
         string GroupKey { get; }
 
         /// <summary>
+        /// Key value for legacy classic effect compatibility. Do not set this for non-classic effects.
+        /// </summary>
+        int ClassicKey { get; }
+
+        /// <summary>
         /// TEXT.RSC ID description for this effect (effect descriptions start at ID 1500).
         /// </summary>
         int TextID { get; }
@@ -52,23 +57,36 @@ namespace DaggerfallWorkshop.Game.Effects
     }
 
     /// <summary>
-    /// Base implementation of a spell effect.
-    /// Spell effects generally perform work against one or more entities (e.g. damage or restore health).
+    /// Base implementation of a magic effect.
+    /// Magic effects are like "actions" for spells, potions, items, advantages, diseases, etc.
+    /// They generally perform work against one or more entities (e.g. damage or restore health).
     /// Some effects perform highly custom operations unique to player (e.g. anchor/teleport UI).
-    /// Effects are scripted in C# so they have full access to engine and UI as required.
-    /// Standard effects are included in build for cross-platform compatibility.
+    /// Magic effects are scripted in C# so they have full access to engine and UI as required.
+    /// Classic magic effects are included in build for cross-platform compatibility.
     /// Custom effects can be added later using mod system (todo:).
     /// </summary>
-    public abstract class BaseSpellEffect : ISpellEffect
+    public abstract class BaseMagicEffect : IMagicEffect
     {
         public abstract string GroupName { get; }
         public abstract string SubGroupName { get; }
         public abstract string GroupKey { get; }
+        public virtual int ClassicKey { get { return 0; } }
         public abstract int TextID { get; }
 
         public virtual TextFile.Token[] CustomText
         {
             get { return null; }
+        }
+
+        public static int MakeClassicKey(byte groupIndex, byte subgroupIndex)
+        {
+            return groupIndex << 8 + subgroupIndex;
+        }
+
+        public static void ReverseClasicKey(int key, out byte groupIndex, out byte subgroupIndex)
+        {
+            groupIndex = (byte)(key >> 8);
+            subgroupIndex = (byte)(key & 0xff);
         }
     }
 }
