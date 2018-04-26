4e1a961cc00e08e089c5c510af0e4df35f7e8346
MeteoricDragon <MattRohde@gmail.com>
2018-04-25 20:02:21 -0500

    Removed unneccessary stuff

--- Assets/BackgroundQuestion.cs
+++ Assets/BackgroundQuestion.cs
@@ -1,49 +1,11 @@
-﻿using DaggerfallWorkshop.Game;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using DaggerfallWorkshop.Game.Entity;
 
 namespace DaggerfallWorkshop.Game
 {
-    [Flags]
-    public enum CareerCheckList
-    {
-        Interkarma = 0,
-        Custom = 1,
-        Mage = 2,
-        Spellsword = 4,
-        Battlemage = 8,
-        Sorcerer = 16,
-        Healer = 32,
-        Nightblade = 64,
-        Bard = 128,
-        Burglar = 256,
-        Rogue = 512,
-        Acrobat = 1024,
-        Thief = 2048,
-        Assassin = 4096,
-        Monk = 8192,
-        Archer = 16384,
-        Ranger = 32768,
-        Barbarian = 65536,
-        Warrior = 131702,
-        Knight = 262144,
-        AllMagicUsers = Custom + Mage + Spellsword + Battlemage + Sorcerer + Healer + Nightblade + Bard,
-        AllNonMagicUsers = Burglar + Rogue + Acrobat + Thief + Assassin + Monk + Archer + Ranger + Barbarian + Warrior + Knight,
-        AllMagi = Custom + Mage + Spellsword + Battlemage + Sorcerer + Healer + Nightblade,
-        AllRogues = Bard + Burglar + Rogue + Acrobat + Thief + Assassin,
-        AllWarriors = Monk + Archer + Ranger + Barbarian + Warrior + Knight,
-        AllClasses = Custom + Mage + Spellsword + Battlemage + Sorcerer + Healer + Nightblade + Bard + 
-            Burglar + Rogue + Acrobat + Thief + Assassin + Monk + Archer + Ranger + Barbarian + Warrior + Knight
-    }
     public class BackgroundQuestion
     {
-        /// <summary>
-        /// The classes that the question is applicable to
-        /// </summary>
-        private CareerCheckList careers;
         private int questionID;
         private string question;
 
@@ -54,7 +16,6 @@ namespace DaggerfallWorkshop.Game
 
         #region Properties
         public int QuestionID  {  get { return questionID; } }
-        public CareerCheckList Careers { get { return careers; } }
         public string Question   {  get { return question; } }
 
         /// <summary>
@@ -93,8 +54,7 @@ namespace DaggerfallWorkshop.Game
         /// <param name="id">a unique ID for the question</param>
         /// <param name="questionText">the text for the question</param>
         /// <param name="answers">an array containing the answers to the question</param>
-        /// <param name="classList">the classes that this question is applicable to</param>
-        public BackgroundQuestion(int id, string questionText, string[] answers, CareerCheckList classList)
+        public BackgroundQuestion(int id, string questionText, string[] answers)
         {
             questionID = id;
             question = questionText;
@@ -105,7 +65,7 @@ namespace DaggerfallWorkshop.Game
                 answerTexts.Add(answerKeys[i], answers[i]); 
                 i++;
             }
-            careers = classList;    
+            //careers = classList;    
 	    }
 
         /// <summary>
@@ -136,14 +96,6 @@ namespace DaggerfallWorkshop.Game
             
             return false;
         }
-        /// <summary>
-        /// Does this question apply to thisClass?
-        /// </summary>
-        /// <param name="thisClass">Class to search for in question</param>
-        /// <returns>returns true if thisClass applies to the question</returns>
-        public bool AppliesToClass(CareerCheckList thisClass)
-        {
-            return ((careers & thisClass) == (thisClass));
-        }
+
     }
 }
--- Assets/BackgroundQuestionList.cs
+++ Assets/BackgroundQuestionList.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using DaggerfallWorkshop.Game;
+using DaggerfallConnect;
+using DaggerfallConnect.Utility;
+using UnityEngine;
+using System.Text.RegularExpressions;
 
 namespace DaggerfallWorkshop.Game
 {
@@ -13,12 +18,7 @@ namespace DaggerfallWorkshop.Game
         public BackgroundQuestionList()
         {
             // add all background questions to List Here:
-            
-            // unit test
-            string[] strings = { "Answer a", "answer b", "Answer c", "answer d", "Answer e", "answer f", "Answer g", "answer h" };
-            List.Add(new BackgroundQuestion(1, "This is a question", strings, CareerCheckList.Acrobat | CareerCheckList.Assassin | CareerCheckList.Healer));
 
-            
         }
 
     }
