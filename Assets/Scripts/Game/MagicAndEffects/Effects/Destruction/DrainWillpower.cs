4e3ea4107f41813a91c28fc6d5e4b0f7a01b0f29
Interkarma <interkarma@dfworkshop.net>
2018-04-27 12:57:03 +1000

    Implement Drain Willpower

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Destruction/DrainWillpower.cs
@@ -0,0 +1,39 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Entity;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Drain - Willpower
+    /// </summary>
+    public class DrainWillpower : DrainEffect
+    {
+        public override void SetProperties()
+        {
+            properties.Key = "Drain-Willpower";
+            properties.ClassicKey = MakeClassicKey(7, 2);
+            properties.GroupName = TextManager.Instance.GetText("ClassicEffects", "drain");
+            properties.SubGroupName = TextManager.Instance.GetText("ClassicEffects", "willpower");
+            properties.SpellMakerDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1521);
+            properties.SpellBookDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1221);
+            properties.SupportMagnitude = true;
+            properties.AllowedTargets = EntityEffectBroker.TargetFlags_Other;
+            properties.AllowedElements = EntityEffectBroker.ElementFlags_All;
+            properties.MagicSkill = DFCareer.MagicSkills.Destruction;
+            properties.MagnitudeCosts = MakeEffectCosts(8, 100, 0.4f, 116);
+            drainStat = DFCareer.Stats.Willpower;
+        }
+    }
+}
