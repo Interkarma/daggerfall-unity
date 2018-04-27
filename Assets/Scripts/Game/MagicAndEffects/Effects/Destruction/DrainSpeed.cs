1b5acf459a46c7aab09eceb07c92c19987b92abc
Interkarma <interkarma@dfworkshop.net>
2018-04-27 13:22:40 +1000

    Implement Drain Speed

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Destruction/DrainSpeed.cs
@@ -0,0 +1,39 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Entity;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Drain - Speed
+    /// </summary>
+    public class DrainSpeed : DrainEffect
+    {
+        public override void SetProperties()
+        {
+            properties.Key = "Drain-Speed";
+            properties.ClassicKey = MakeClassicKey(7, 6);
+            properties.GroupName = TextManager.Instance.GetText("ClassicEffects", "drain");
+            properties.SubGroupName = TextManager.Instance.GetText("ClassicEffects", "speed");
+            properties.SpellMakerDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1525);
+            properties.SpellBookDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1224);
+            properties.SupportMagnitude = true;
+            properties.AllowedTargets = EntityEffectBroker.TargetFlags_Other;
+            properties.AllowedElements = EntityEffectBroker.ElementFlags_All;
+            properties.MagicSkill = DFCareer.MagicSkills.Destruction;
+            properties.MagnitudeCosts = MakeEffectCosts(8, 100, 0.4f, 116);
+            drainStat = DFCareer.Stats.Speed;
+        }
+    }
+}
