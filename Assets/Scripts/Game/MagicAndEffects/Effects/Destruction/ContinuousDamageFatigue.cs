7a45632cf4f73e03455cbf8aa2625214fb3a75c4
ifkopifko <39078806+ifkopifko@users.noreply.github.com>
2018-05-10 09:42:07 +0200

    Update ContinuousDamageFatigue.cs

--- Assets/Scripts/Game/MagicAndEffects/Effects/Destruction/ContinuousDamageFatigue.cs
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Destruction/ContinuousDamageFatigue.cs
@@ -33,8 +33,8 @@ namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
             properties.AllowedTargets = EntityEffectBroker.TargetFlags_Other;
             properties.AllowedElements = EntityEffectBroker.ElementFlags_All;
             properties.MagicSkill = DFCareer.MagicSkills.Destruction;
-            properties.DurationCosts = MakeEffectCosts(20, 8, 1, 68);
-            properties.MagnitudeCosts = MakeEffectCosts(40, 28, 2, 28);
+            properties.DurationCosts = MakeEffectCosts(20, 8);
+            properties.MagnitudeCosts = MakeEffectCosts(40, 28);
         }
 
         public override void MagicRound()
