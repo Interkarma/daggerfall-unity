f290a36d1f33dc9eeb0fb7ef7e1e93a4ac5d46c3
Interkarma <interkarma@dfworkshop.net>
2018-05-10 11:11:37 +1000

    Stub DiseaseEffect base
    This class will provide base functionality for diseases to operate within effect system.
    @allofich You've done some incredible work in reverse engineering diseases and preparing code while waiting for the effect system to mature to this point. I'll not touch your code for now and just spec out a disease or two in effect system to make sure everything is working around duration tracking, applying effect mods, serialization, etc. Once we're both happy, we can look at wiring up diseases to effect system fully. Consider this a work in progress for now while I'm still building out effect system core.

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Diseases/DiseaseEffect.cs
@@ -0,0 +1,77 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using System;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Entity;
+using FullSerializer;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Disease effect base.
+    /// </summary>
+    public abstract class Disease : IncumbentEffect
+    {
+        const string textDatabase = "Diseases";
+
+        int forcedRoundsRemaining = 1;
+
+        // Disease effects are permanent until cured so we manage our own lifecycle
+        protected override int RemoveRound()
+        {
+            return forcedRoundsRemaining;
+        }
+
+        // Always present at least one round remaining so effect system does not remove
+        public override int RoundsRemaining
+        {
+            get { return forcedRoundsRemaining; }
+        }
+
+        protected override void BecomeIncumbent()
+        {
+        }
+
+        protected override void AddState(IncumbentEffect incumbent)
+        {
+            if (forcedRoundsRemaining == 0)
+                return;
+        }
+
+        #region Serialization
+
+        [fsObject("v1")]
+        public struct SaveData_v1
+        {
+            public int forcedRoundsRemaining;
+        }
+
+        public override object GetSaveData()
+        {
+            SaveData_v1 data = new SaveData_v1();
+            data.forcedRoundsRemaining = forcedRoundsRemaining;
+
+            return data;
+        }
+
+        public override void RestoreSaveData(object dataIn)
+        {
+            SaveData_v1 data = (SaveData_v1)dataIn;
+            if (dataIn == null)
+                return;
+
+            forcedRoundsRemaining = data.forcedRoundsRemaining;
+        }
+
+        #endregion
+    }
+}
