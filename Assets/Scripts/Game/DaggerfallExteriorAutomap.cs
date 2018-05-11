82e8dc2019aae29a317d87dc1db45cfdaa75e2f8
Nystul-the-Magician <11569250+Nystul-the-Magician@users.noreply.github.com>
2018-05-11 14:35:10 +0200

    only shows the tg, dbh guild nameplate when player has joined them due to the new filtering this had to be handled specially

--- Assets/Scripts/Game/DaggerfallExteriorAutomap.cs
+++ Assets/Scripts/Game/DaggerfallExteriorAutomap.cs
@@ -764,8 +764,10 @@ namespace DaggerfallWorkshop.Game
                             PlayerGPS.DiscoveredBuilding discoveredBuilding;
                             if (GameManager.Instance.PlayerGPS.GetDiscoveredBuilding(buildingSummary.buildingKey, out discoveredBuilding))
                             {
-                                
-                                if (!RMBLayout.IsResidence(buildingSummary.BuildingType) || buildingSummary.FactionId == Guilds.ThievesGuild.FactionId || buildingSummary.FactionId == Guilds.DarkBrotherhood.FactionId) // guilds, shops, taverns, palace (general case)
+                                // show guilds, shops, taverns, palace (general case), show thieves guild and dark brotherhood guild halls if member
+                                if (!RMBLayout.IsResidence(buildingSummary.BuildingType) ||
+                                    (buildingSummary.FactionId == Guilds.ThievesGuild.FactionId && GameManager.Instance.GuildManager.HasJoined(FactionFile.GuildGroups.GeneralPopulace)) ||
+                                    (buildingSummary.FactionId == Guilds.DarkBrotherhood.FactionId && GameManager.Instance.GuildManager.HasJoined(FactionFile.GuildGroups.DarkBrotherHood)))
                                 {
                                     newBuildingNameplate.name = discoveredBuilding.displayName;
                                 }
