8bd40f69c49b8e558cfa7e5464f2c1a9a5e074b2
Interkarma <interkarma@dfworkshop.net>
2018-05-13 18:02:54 +1000

    Implement FortifyIntelligence

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Restoration/FortifyIntelligence.cs
@@ -0,0 +1,42 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Entity;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Fortify Attribute - Intelligence
+    /// </summary>
+    public class FortifyIntelligence : FortifyEffect
+    {
+        public override void SetProperties()
+        {
+            properties.Key = "Fortify-Intelligence";
+            properties.ClassicKey = MakeClassicKey(9, 1);
+            properties.GroupName = TextManager.Instance.GetText("ClassicEffects", "fortifyAttribute");
+            properties.SubGroupName = TextManager.Instance.GetText("ClassicEffects", "intelligence");
+            properties.SpellMakerDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1533);
+            properties.SpellBookDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1233);
+            properties.SupportDuration = true;
+            properties.SupportMagnitude = true;
+            properties.AllowedTargets = EntityEffectBroker.TargetFlags_All;
+            properties.AllowedElements = EntityEffectBroker.ElementFlags_MagicOnly;
+            properties.AllowedCraftingStations = MagicCraftingStations.SpellMaker;
+            properties.MagicSkill = DFCareer.MagicSkills.Restoration;
+            properties.DurationCosts = MakeEffectCosts(28, 100);
+            properties.MagnitudeCosts = MakeEffectCosts(40, 120);
+            fortifyStat = DFCareer.Stats.Intelligence;
+        }
+    }
+}
