bbe550683291a102e42cc69f77574299128a288a
MeteoricDragon <MattRohde@gmail.com>
2018-05-16 20:57:05 -0500

    Finish Fortify Effects
    Implements the remaining 5 fortify effects, first 3 authored by Interkarma, Last 5 copy & pasted (mostly) by me.

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Restoration/FortifyEndurance.cs
@@ -0,0 +1,42 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Entity;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Fortify Attribute - Endurance
+    /// </summary>
+    public class FortifyEndurance : FortifyEffect
+    {
+        public override void SetProperties()
+        {
+            properties.Key = "Fortify-Endurance";
+            properties.ClassicKey = MakeClassicKey(9, 4);
+            properties.GroupName = TextManager.Instance.GetText("ClassicEffects", "fortifyAttribute");
+            properties.SubGroupName = TextManager.Instance.GetText("ClassicEffects", "endurance");
+            properties.SpellMakerDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1536);
+            properties.SpellBookDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1236);
+            properties.SupportDuration = true;
+            properties.SupportMagnitude = true;
+            properties.AllowedTargets = EntityEffectBroker.TargetFlags_All;
+            properties.AllowedElements = EntityEffectBroker.ElementFlags_MagicOnly;
+            properties.AllowedCraftingStations = MagicCraftingStations.SpellMaker;
+            properties.MagicSkill = DFCareer.MagicSkills.Restoration;
+            properties.DurationCosts = MakeEffectCosts(28, 100);
+            properties.MagnitudeCosts = MakeEffectCosts(40, 120);
+            fortifyStat = DFCareer.Stats.Endurance;
+        }
+    }
+}
