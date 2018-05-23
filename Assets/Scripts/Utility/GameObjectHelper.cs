0e65c394ea7c7f75adda43ff4f28215a7f5b883b
Interkarma <interkarma@dfworkshop.net>
2018-05-23 12:30:00 +1000

    Set context on new lootable corpse markers

--- Assets/Scripts/Utility/GameObjectHelper.cs
+++ Assets/Scripts/Utility/GameObjectHelper.cs
@@ -704,6 +704,7 @@ namespace DaggerfallWorkshop.Utility
             loot.LoadID = loadID;
             loot.customDrop = true;
             loot.playerOwned = false;
+            loot.WorldContext = playerEnterExit.WorldContext;
 
             // If dropped outside ask StreamingWorld to track loose object
             if (!GameManager.Instance.IsPlayerInside)
