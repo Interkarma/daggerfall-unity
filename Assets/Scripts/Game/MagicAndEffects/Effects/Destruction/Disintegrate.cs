7b75e02f5e505a2003211b3f768b889db8abc176
Interkarma <interkarma@dfworkshop.net>
2018-05-28 17:46:41 +1000

    Implement Disintegrate

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Destruction/Disintegrate.cs
@@ -0,0 +1,51 @@
+﻿// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Entity;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Disintegrate
+    /// </summary>
+    public class Disintegrate : BaseEntityEffect
+    {
+        public override void SetProperties()
+        {
+            properties.Key = "Disintegrate";
+            properties.ClassicKey = MakeClassicKey(5, 255);
+            properties.GroupName = TextManager.Instance.GetText("ClassicEffects", "disintegrate");
+            properties.SpellMakerDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1515);
+            properties.SpellBookDescription = DaggerfallUnity.Instance.TextProvider.GetRSCTokens(1215);
+            properties.SupportChance = true;
+            properties.AllowedTargets = EntityEffectBroker.TargetFlags_Other;
+            properties.AllowedElements = EntityEffectBroker.ElementFlags_All;
+            properties.AllowedCraftingStations = MagicCraftingStations.SpellMaker;
+            properties.MagicSkill = DFCareer.MagicSkills.Destruction;
+            properties.ChanceCosts = MakeEffectCosts(80, 140);
+        }
+
+        public override void MagicRound()
+        {
+            base.MagicRound();
+
+            // Get peered entity gameobject
+            DaggerfallEntityBehaviour entityBehaviour = GetPeeredEntityBehaviour(manager);
+            if (!entityBehaviour)
+                return;
+
+            // Kill target
+            entityBehaviour.Entity.SetHealth(0);
+        }
+    }
+}
