7e8a0e37a507e025f5a11d3f5c49c281c4d18289
MeteoricDragon <MattRohde@gmail.com>
2018-07-17 17:04:31 -0500

    Add Vitals Loss indicator system
    Each of the player's vitals on the hud now have a bar that visually and actively shows how much is lost of each bar after it happens.
    
    If the player loses health, after the health bar updates, it exposes the indicator bar that was hiding behind the health bar, then the quickly and smoothly shrinks and hides behind the health bar.

--- Assets/Scripts/Game/UserInterface/HUDVitals.cs
+++ Assets/Scripts/Game/UserInterface/HUDVitals.cs
@@ -9,6 +9,9 @@
 // Notes:
 //
 
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.Formulas;
+using DaggerfallWorkshop.Game.Serialization;
 using UnityEngine;
 
 namespace DaggerfallWorkshop.Game.UserInterface
@@ -26,11 +29,18 @@ namespace DaggerfallWorkshop.Game.UserInterface
         const int nativeBarHeight = 32;
         public const int borderSize = 10;
 
+        VitalsChangeDetector vitalsDetector = GameManager.Instance.VitalsChangeDetector;
         VerticalProgress healthProgress = new VerticalProgress();
         VerticalProgress fatigueProgress = new VerticalProgress();
         VerticalProgress magickaProgress = new VerticalProgress();
         VerticalProgress breathProgress = new VerticalProgress();
+        VerticalProgressIndicator healthProgressLoss = new VerticalProgressIndicator();
+        VerticalProgressIndicator fatigueProgressLoss = new VerticalProgressIndicator();
+        VerticalProgressIndicator magickaProgressLoss = new VerticalProgressIndicator();
 
+        Color healthLossColor = new Color(0, 0.22f, 0);
+        Color fatigueLossColor = new Color(0.44f, 0, 0);
+        Color magickaLossColor = new Color(0, 0, 0.44f);
         /// <summary>
         /// Gets or sets current health as value between 0 and 1.
         /// </summary>
@@ -80,11 +90,20 @@ namespace DaggerfallWorkshop.Game.UserInterface
             healthProgress.VerticalAlignment = VerticalAlignment.Bottom;
             fatigueProgress.VerticalAlignment = VerticalAlignment.Bottom;
             magickaProgress.VerticalAlignment = VerticalAlignment.Bottom;
+            healthProgressLoss.VerticalAlignment = VerticalAlignment.Bottom;
+            fatigueProgressLoss.VerticalAlignment = VerticalAlignment.Bottom;
+            magickaProgressLoss.VerticalAlignment = VerticalAlignment.Bottom;
 
+            // to make bar appear behind other bars, add it first.
+            Components.Add(healthProgressLoss);
+            Components.Add(fatigueProgressLoss);
+            Components.Add(magickaProgressLoss);
             Components.Add(healthProgress);
             Components.Add(fatigueProgress);
             Components.Add(magickaProgress);
             Components.Add(breathProgress);
+
+            SaveLoadManager.OnStartLoad += SaveLoadManager_OnStartLoad;
         }
 
         public override void Update()
@@ -99,6 +118,26 @@ namespace DaggerfallWorkshop.Game.UserInterface
                 float breathBarHeight = GameManager.Instance.PlayerEntity.Stats.LiveEndurance * Scale.y;
 
                 Size = new Vector2(barWidth * 5, barHeight);
+                
+                
+                if (vitalsDetector.HealthLost != 0)
+                    healthProgressLoss.BeginSmoothChange(GameManager.Instance.PlayerEntity.CurrentHealthPercent);
+                if (vitalsDetector.FatigueLost != 0)
+                    fatigueProgressLoss.BeginSmoothChange(GameManager.Instance.PlayerEntity.CurrentFatigue / GameManager.Instance.PlayerEntity.MaxFatigue);
+                if (vitalsDetector.MagickaLost != 0)
+                    magickaProgressLoss.BeginSmoothChange(GameManager.Instance.PlayerEntity.CurrentMagicka / GameManager.Instance.PlayerEntity.MaxMagicka);
+
+                healthProgressLoss.Position = new Vector2(0, 0);
+                healthProgressLoss.Size = new Vector2(barWidth, barHeight);
+                healthProgressLoss.Cycle();
+
+                fatigueProgressLoss.Position = new Vector2(barWidth * 2, 0);
+                fatigueProgressLoss.Size = new Vector2(barWidth, barHeight);
+                fatigueProgressLoss.Cycle();
+
+                magickaProgressLoss.Position = new Vector2(barWidth * 4, 0);
+                magickaProgressLoss.Size = new Vector2(barWidth, barHeight);
+                magickaProgressLoss.Cycle();
 
                 healthProgress.Position = new Vector2(0, 0);
                 healthProgress.Size = new Vector2(barWidth, barHeight);
@@ -120,13 +159,18 @@ namespace DaggerfallWorkshop.Game.UserInterface
             {
                 healthProgress.ProgressTexture = DaggerfallUI.GetTextureFromImg(fatigueBarFilename);
                 fatigueProgress.ProgressTexture = DaggerfallUI.GetTextureFromImg(healthBarFilename);
+                healthProgressLoss.Color = fatigueLossColor;
+                fatigueProgressLoss.Color = healthLossColor;
             }
             else
             {
                 healthProgress.ProgressTexture = DaggerfallUI.GetTextureFromImg(healthBarFilename);
                 fatigueProgress.ProgressTexture = DaggerfallUI.GetTextureFromImg(fatigueBarFilename);
+                healthProgressLoss.Color = healthLossColor;
+                fatigueProgressLoss.Color = fatigueLossColor;
             }
             magickaProgress.ProgressTexture = DaggerfallUI.GetTextureFromImg(magickaBarFilename);
+            magickaProgressLoss.Color = magickaLossColor;
         }
 
         void SetRemainingBreath(float amount)
@@ -137,6 +181,23 @@ namespace DaggerfallWorkshop.Game.UserInterface
                 breathProgress.Color = new Color32(148, 12, 0, 255);
             else
                 breathProgress.Color = new Color32(247, 239, 41, 255);
+        }
+
+        private void SaveLoadManager_OnStartLoad(SaveData_v1 saveData)
+        {
+            PlayerEntityData_v1 pData = saveData.playerData.playerEntity;
+
+            // sync health bar
+            healthProgressLoss.Amount = pData.currentHealth / (float)pData.maxHealth;
+
+            // sync fatigue bar
+            int maxFatigue = (pData.stats.LiveStrength + pData.stats.LiveEndurance) * 64;
+            fatigueProgressLoss.Amount = pData.currentFatigue / (float)maxFatigue;
+
+            // sync magicka bar
+            DFCareer career = pData.careerTemplate;
+            int maxMagicka = FormulaHelper.SpellPoints(pData.stats.LiveIntelligence, career.SpellPointMultiplierValue);
+            magickaProgressLoss.Amount = pData.currentMagicka / (float)maxMagicka;
         }
     }
 }
---
+++ Assets/Scripts/Game/UserInterface/VerticalProgressIndicator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using DaggerfallWorkshop.Game.Serialization;
+
+namespace DaggerfallWorkshop.Game.UserInterface
+{
+    /// <summary>
+    /// A vertical progress/indicator bar. Modified to smoothly change
+    /// </summary>
+    public class VerticalProgressIndicator : VerticalProgress
+    {
+        private float prevPercent;
+        private float targetPercent;
+        private float timer;
+        private const float timerMax = 0.4f;
+        private bool cycleTimer;
+
+        public void BeginSmoothChange(float target)
+        {
+            cycleTimer = true;
+            timer = -0.5f;
+            prevPercent = Amount;
+            targetPercent = target;
+            if (targetPercent > prevPercent)
+                timer = timerMax;
+        }
+
+        public void Cycle()
+        {
+            if (!cycleTimer)
+                return;       
+            
+            timer += Time.deltaTime;
+
+            if (timer > 0)
+            {
+                float t = Mathf.Clamp01(timer / timerMax);
+                Amount = Mathf.Lerp(prevPercent, targetPercent, t);
+
+                if (timer >= timerMax)
+                    cycleTimer = false;
+            }
+        }
+    }
+}
