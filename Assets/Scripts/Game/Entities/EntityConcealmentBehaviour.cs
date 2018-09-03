d17ab60b3a57a3f4284ee1a0570bbcb8df7dddc7
Interkarma <interkarma@dfworkshop.net>
2018-09-04 07:46:10 +1000

    Implement magical concealment class for mobiles
    Works on mobile enemies and NPCs. Just a simple concealment check that toggles meshrender for billboard.
    For mobile enemies, they will break normal power concealment on attack.

---
+++ Assets/Scripts/Game/Entities/EntityConcealmentBehaviour.cs
@@ -0,0 +1,64 @@
+// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using UnityEngine;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.MagicAndEffects;
+
+namespace DaggerfallWorkshop.Game.Entity
+{
+    /// <summary>
+    /// Handles magical concealment for entities other than player.
+    /// Inherit from this class for custom entity concealment handling.
+    /// </summary>
+    [RequireComponent(typeof(DaggerfallEntityBehaviour))]
+    [RequireComponent(typeof(EntityEffectManager))]
+    public class EntityConcealmentBehaviour : MonoBehaviour
+    {
+        protected DaggerfallEntityBehaviour entityBehaviour;
+        protected EntityEffectManager entityEffectManager;
+        protected MeshRenderer meshRenderer;
+
+        #region Unity
+
+        void Awake()
+        {
+            CacheReferences();
+        }
+
+        void Update()
+        {
+            bool isConcealed = (entityBehaviour && entityBehaviour.Entity.IsMagicallyConcealed);
+            MakeConcealed(isConcealed);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected void CacheReferences()
+        {
+            entityBehaviour = GetComponent<DaggerfallEntityBehaviour>();
+            entityEffectManager = GetComponent<EntityEffectManager>();
+            meshRenderer = GetComponentInChildren<MeshRenderer>();
+        }
+
+        protected void MakeConcealed(bool concealed)
+        {
+            if (meshRenderer)
+            {
+                meshRenderer.enabled = !concealed;
+            }
+        }
+
+        #endregion
+    }
+}
