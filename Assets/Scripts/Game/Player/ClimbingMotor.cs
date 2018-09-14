9f33858424015d2b3eaeea99d41d8aac28a69ed4
MeteoricDragon <MattRohde@gmail.com>
2018-09-13 19:08:16 -0500

    Implement Corner check system
    Implements a recursive raycast system that checks the direction the player is moving for MeshColliders.  rotates counterclockwise if moving right, clockwise if moving left, 90 degrees, and checks again, then rotates and checks again.  Searching for a RaycastHit to find the normal of the target transfer wall.   This system is essential to find the player's transfer wall.

--- Assets/Scripts/Game/Player/ClimbingMotor.cs
+++ Assets/Scripts/Game/Player/ClimbingMotor.cs
@@ -34,6 +34,8 @@ namespace DaggerfallWorkshop.Game
         // minimum percent chance to continue climbing per skill check, gets closer to 100 with higher skill
         private const int continueClimbMinChance = 70;
         private const int graspWallMinChance = 50;
+        private int FindWallLoopCount;
+
         public bool IsClimbing
         {
             get { return isClimbing; }
@@ -200,6 +202,113 @@ namespace DaggerfallWorkshop.Game
                 ledgeDirection = Vector3.zero;
         }
 
+        //Find the line of intersection between two planes.
+        //The inputs are two game objects which represent the planes.
+        //The outputs are a point on the line and a vector which indicates it's direction.
+        void planePlaneIntersection(out Vector3 linePoint, out Vector3 lineVec, GameObject plane1, GameObject plane2)
+        {
+            linePoint = Vector3.zero;
+            lineVec = Vector3.zero;
+
+            //Get the normals of the planes.
+            Vector3 plane1Normal = plane1.transform.up;
+            Vector3 plane2Normal = plane2.transform.up;
+
+            //We can get the direction of the line of intersection of the two planes by calculating the
+            //cross product of the normals of the two planes. Note that this is just a direction and the
+            //line is not fixed in space yet.
+            lineVec = Vector3.Cross(plane1Normal, plane2Normal);
+
+            //Next is to calculate a point on the line to fix it's position. This is done by finding a vector from
+            //the plane2 location, moving parallel to it's plane, and intersecting plane1. To prevent rounding
+            //errors, this vector also has to be perpendicular to lineDirection. To get this vector, calculate
+            //the cross product of the normal of plane2 and the lineDirection.      
+            Vector3 ldir = Vector3.Cross(plane2Normal, lineVec);
+
+            float numerator = Vector3.Dot(plane1Normal, ldir);
+
+            //Prevent divide by zero.
+            if (Mathf.Abs(numerator) > 0.000001f)
+            {
+
+                Vector3 plane1ToPlane2 = plane1.transform.position - plane2.transform.position;
+                float t = Vector3.Dot(plane1Normal, plane1ToPlane2) / numerator;
+                linePoint = plane2.transform.position + t * ldir;
+            }
+        }
+        /// <summary>
+        /// Find the Closest point on a line to a given point in space
+        /// </summary>
+        /// <param name="vA">Point A of line</param>
+        /// <param name="vB">Point B of line</param>
+        /// <param name="vPoint">The point to measure shortest distance from</param>
+        /// <returns>A point on given line where it is perpendicular to vPoint</returns>
+        private Vector3 ClosestPointOnLine(Vector3 vA, Vector3 vB, Vector3 vPoint)
+        {
+            var vVector1 = vPoint - vA;
+            var vVector2 = (vB - vA).normalized;
+
+            var d = Vector3.Distance(vA, vB);
+            var t = Vector3.Dot(vVector2, vVector1);
+
+            if (t <= 0)
+                return vA;
+
+            if (t >= d)
+                return vB;
+
+            var vVector3 = vVector2 * t;
+
+            var vClosestPoint = vA + vVector3;
+
+            return vClosestPoint;
+        }
+        
+        // first ray = 90deg, rotate left 90deg
+        //distance = controller.radius + 0.10f;
+        private bool FindAdjacentWall(Vector3 origin, Vector3 direction, bool searchClockwise)
+        {
+            RaycastHit hit;
+            float distance = direction.magnitude;
+
+            // use recursion to raycast vectors to find the adjacent wall
+            Debug.DrawRay(origin, direction, Color.green, Time.deltaTime);
+            if (Physics.Raycast(origin, direction, out hit, distance) 
+                && (hit.collider.gameObject.GetComponent<MeshCollider>() != null))
+            {
+                // need to assign the found wall's normal to a member level variable
+                Debug.DrawRay(hit.point, hit.normal);
+
+                FindWallLoopCount = 0;
+                return true;
+            }
+            else
+            {
+                FindWallLoopCount++;
+                if (FindWallLoopCount < 3)
+                {
+                    // find next vector info now
+                    Vector3 lastOrigin = origin;
+                    origin = origin + direction;
+                    Vector3 nextDirection = Vector3.zero; 
+
+                    if (searchClockwise)
+                    {
+                        nextDirection = Vector3.Cross(lastOrigin - origin, Vector3.up).normalized * distance;
+                    }
+                    else
+                    {
+                        nextDirection = Vector3.Cross(Vector3.up, lastOrigin - origin).normalized * distance;
+                    }
+
+                    return FindAdjacentWall(origin, nextDirection, searchClockwise);
+                }
+                FindWallLoopCount = 0;
+                return false;
+            }
+
+        }
+
         /// <summary>
         /// Perform Climbing Movement and call Skill Checks
         /// </summary>
@@ -212,6 +321,11 @@ namespace DaggerfallWorkshop.Game
 
             // Climbing effect states "target can climb twice as well" - doubling climbing speed
             float climbingBoost = player.IsEnhancedClimbing ? 2f : 1f;
+            // if strafing to either side, this will be set so we can check for wrap-around corners.
+            Vector3 checkDirection = Vector3.zero;
+            bool cornerFound = false;
+            bool outsideCornerFound = false; // experimental
+            bool insideCornerFound = false; // experimental
 
             if (!isSlipping)
             {
@@ -225,10 +339,23 @@ namespace DaggerfallWorkshop.Game
                     else if (InputManager.Instance.HasAction(InputManager.Actions.MoveBackwards))
                         moveDirection.y = Vector3.down.y * climbScalar;
 
+                    float checkScalar = controller.radius + 0.5f;
+                    Vector3 awayAdjustment = ledgeDirection * 0.30f;
                     if (InputManager.Instance.HasAction(InputManager.Actions.MoveRight))
-                        moveDirection += Vector3.Cross(Vector3.up, ledgeDirection).normalized * climbScalar;
+                    {
+                        checkDirection = Vector3.Cross(Vector3.up, ledgeDirection).normalized;
+                        // perform check for adjacent wall
+                        cornerFound = FindAdjacentWall(controller.transform.position, checkDirection * checkScalar, false);
+
+                        moveDirection += checkDirection * climbScalar;
+                    }
                     else if (InputManager.Instance.HasAction(InputManager.Actions.MoveLeft))
-                        moveDirection += Vector3.Cross(ledgeDirection, Vector3.up).normalized * climbScalar;
+                    {
+                        checkDirection = Vector3.Cross(ledgeDirection, Vector3.up).normalized;
+                        // perform check for adjacent wall before moving
+                        cornerFound = FindAdjacentWall(controller.transform.position, checkDirection * checkScalar, true);
+                        moveDirection += checkDirection * climbScalar;
+                    }
                 }
                 else
                     moveDirection.y = Vector3.up.y * climbScalar;
@@ -238,6 +365,9 @@ namespace DaggerfallWorkshop.Game
                 acrobatMotor.CheckInitFall();
                 acrobatMotor.ApplyGravity(ref moveDirection);
             }
+
+            /* Did we find an outside or inside corner? If so we must override wasd 
+             * movement and make rotational movement */
 
             controller.Move(moveDirection * Time.deltaTime);
             playerMotor.CollisionFlags = controller.collisionFlags;
