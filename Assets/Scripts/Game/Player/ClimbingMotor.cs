44900c20041c9012caa4224cbb8626e1ef1041a9
MeteoricDragon <MattRohde@gmail.com>
2018-09-15 13:48:13 -0500

    remove unnecessary functions

--- Assets/Scripts/Game/Player/ClimbingMotor.cs
+++ Assets/Scripts/Game/Player/ClimbingMotor.cs
@@ -211,67 +211,6 @@ namespace DaggerfallWorkshop.Game
             
         }
 
-        //Find the line of intersection between two planes.
-        //The inputs are two game objects which represent the planes.
-        //The outputs are a point on the line and a vector which indicates it's direction.
-        void planePlaneIntersection(out Vector3 linePoint, out Vector3 lineVec, GameObject plane1, GameObject plane2)
-        {
-            linePoint = Vector3.zero;
-            lineVec = Vector3.zero;
-
-            //Get the normals of the planes.
-            Vector3 plane1Normal = plane1.transform.up;
-            Vector3 plane2Normal = plane2.transform.up;
-
-            //We can get the direction of the line of intersection of the two planes by calculating the
-            //cross product of the normals of the two planes. Note that this is just a direction and the
-            //line is not fixed in space yet.
-            lineVec = Vector3.Cross(plane1Normal, plane2Normal);
-
-            //Next is to calculate a point on the line to fix it's position. This is done by finding a vector from
-            //the plane2 location, moving parallel to it's plane, and intersecting plane1. To prevent rounding
-            //errors, this vector also has to be perpendicular to lineDirection. To get this vector, calculate
-            //the cross product of the normal of plane2 and the lineDirection.      
-            Vector3 ldir = Vector3.Cross(plane2Normal, lineVec);
-
-            float numerator = Vector3.Dot(plane1Normal, ldir);
-
-            //Prevent divide by zero.
-            if (Mathf.Abs(numerator) > 0.000001f)
-            {
-
-                Vector3 plane1ToPlane2 = plane1.transform.position - plane2.transform.position;
-                float t = Vector3.Dot(plane1Normal, plane1ToPlane2) / numerator;
-                linePoint = plane2.transform.position + t * ldir;
-            }
-        }
-        /// <summary>
-        /// Find the Closest point on a line to a given point in space
-        /// </summary>
-        /// <param name="vA">Point A of line</param>
-        /// <param name="vB">Point B of line</param>
-        /// <param name="vPoint">The point to measure shortest distance from</param>
-        /// <returns>A point on given line where it is perpendicular to vPoint</returns>
-        private Vector3 ClosestPointOnLine(Vector3 vA, Vector3 vB, Vector3 vPoint)
-        {
-            var vVector1 = vPoint - vA;
-            var vVector2 = (vB - vA).normalized;
-
-            var d = Vector3.Distance(vA, vB);
-            var t = Vector3.Dot(vVector2, vVector1);
-
-            if (t <= 0)
-                return vA;
-
-            if (t >= d)
-                return vB;
-
-            var vVector3 = vVector2 * t;
-
-            var vClosestPoint = vA + vVector3;
-
-            return vClosestPoint;
-        }
         //Calculate the intersection point of two lines. Returns true if lines intersect, otherwise false.
         //Note that in 3d, two lines do not intersect most of the time. So if the two lines are not in the 
         //same plane, use ClosestPointsOnTwoLines() instead.
