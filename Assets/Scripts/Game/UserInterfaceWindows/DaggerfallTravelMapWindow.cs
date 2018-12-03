7dda2c4ae6df2e08454db4125bcb9b729bb0a0ca
Lypyl <lypyldf@gmail.com>
2018-12-03 02:02:50 -0500

    Adds methods to DaggerfallTravelMapWindow to assist with saving & loading state
    Also removes the previous change to DaggerfallTravelMapWindow.OnPush() that was intended to clear the fast travel settings as this isn't needed

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallTravelMapWindow.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallTravelMapWindow.cs
@@ -23,6 +23,7 @@ using System.Collections.Generic;
 using Wenzil.Console;
 using Wenzil.Console.Commands;
 using DaggerfallWorkshop.Game.Utility;
+using DaggerfallWorkshop.Game.Serialization;
 
 namespace DaggerfallWorkshop.Game.UserInterfaceWindows
 {
@@ -286,14 +287,6 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
             {
                 SetPlayerRegionOverlay();
                 CloseRegionPanel();
-            }
-            else
-            {
-                //clear travel settings pop up on load event
-                Serialization.SaveLoadManager.OnLoad += (saveData) =>
-                {
-                    popUp = null;
-                };
             }
 
         }
@@ -1225,6 +1218,46 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
                 townsFilterButton.BackgroundTexture = townsFilterButtonDisabled;
         }
 
+        public TravelMapSaveData GetTravelMapSaveData()
+        {
+            TravelMapSaveData data = new TravelMapSaveData();
+            data.filterDungeons = filterDungeons;
+            data.filterHomes = filterHomes;
+            data.filterTemples = filterTemples;
+            data.filterTowns = filterTowns;
+
+            if(popUp != null)
+            {
+                data.sleepInn = popUp.SleepModeInn;
+                data.speedCautious = popUp.SpeedCautious;
+                data.travelShip = popUp.TravelShip;
+            }
+
+            return data;
+        }
+
+        public void SetTravelMapFromSaveData(TravelMapSaveData data)
+        {
+            //if doesn't have save data, use defaults
+            if(data == null)
+                data = new TravelMapSaveData();
+
+            filterDungeons = data.filterDungeons;
+            filterHomes = data.filterHomes;
+            filterTemples = data.filterTemples;
+            filterTowns = data.filterTowns;
+
+            if(popUp == null)
+            {
+                popUp = new DaggerfallTravelPopUp(DaggerfallUI.UIManager, this, this);
+            }
+
+            popUp.SleepModeInn = data.sleepInn;
+            popUp.SpeedCautious = data.speedCautious;
+            popUp.TravelShip = data.travelShip;
+
+            UpdateSearchButtons();
+        }
         #endregion
 
         #region Helper Methods
@@ -1559,6 +1592,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
                 }
             }
         }
+
 
         #endregion
 
