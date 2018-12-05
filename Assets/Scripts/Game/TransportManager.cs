Interkarma/daggerfall-unity

commit 60d083eb5058952fe0f640fa33142e8be378d4b6

Make all sound effects respect volume setting

Assets/Scripts/Game/TransportManager.cs
@@ -146,7 +146,10 @@ namespace DaggerfallWorkshop.Game
                     }
 
                     if (!ridingAudioSource.isPlaying)
+                    {
+                        ridingAudioSource.volume = DaggerfallUnity.Settings.SoundVolume;
                         ridingAudioSource.Play();
+                    }
                 }
                 // Time for a whinney?
                 if (neighTime < Time.time)

Assets/Scripts/Game/AmbientEffectsPlayer.cs
@@ -95,6 +95,9 @@ namespace DaggerfallWorkshop.Game
                 StartWaiting();
             }
 
+            // Update sound volume
+            dfAudioSource.AudioSource.volume = DaggerfallUnity.Settings.SoundVolume;
+
             // Start rain loop if not running
             if ((Presets == AmbientSoundPresets.Rain || Presets == AmbientSoundPresets.Storm) && rainLoop == null)
             {

Assets/Scripts/Game/DaggerfallUI.cs
@@ -709,7 +709,7 @@ namespace DaggerfallWorkshop.Game
         public void PlayOneShot(AudioClip clip)
         {
             if (audioSource)
-                audioSource.PlayOneShot(clip);
+                audioSource.PlayOneShot(clip, DaggerfallUnity.Settings.SoundVolume);
         }
 
         public void PlayOneShot(SoundClips clip)

Assets/Scripts/Game/EnemySounds.cs
@@ -107,7 +107,7 @@ namespace DaggerfallWorkshop.Game
 
                 // Play attack sound only about half the time
                 if (Random.value > 0.5f)
-                    dfAudioSource.AudioSource.PlayOneShot(attackClip);
+                    dfAudioSource.AudioSource.PlayOneShot(attackClip, volumeScale * DaggerfallUnity.Settings.SoundVolume);
             }
         }
 

Assets/Scripts/Game/Questing/Actions/PlaySound.cs
@@ -118,7 +118,7 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
                 var source = QuestMachine.Instance.GetComponent<AudioSource>();
                 if (source != null && !source.isPlaying)
                 {
-                    source.PlayOneShot(clip);
+                    source.PlayOneShot(clip, DaggerfallUnity.Settings.SoundVolume);
                     lastTimePlayed = gameSeconds;
                 }
             }

Assets/Scripts/Game/UserInterfaceWindows/CreateCharClassSelect.cs
@@ -87,7 +87,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
                 uiManager.PushWindow(messageBox);
 
                 AudioClip clip = DaggerfallUnity.Instance.SoundReader.GetAudioClip(SoundClips.SelectClassDrums);
-                DaggerfallUI.Instance.AudioSource.PlayOneShot(clip);
+                DaggerfallUI.Instance.AudioSource.PlayOneShot(clip, DaggerfallUnity.Settings.SoundVolume);
             }
         }
 

Assets/Scripts/Game/UserInterfaceWindows/CreateCharRaceSelect.cs
@@ -112,7 +112,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
                 uiManager.PushWindow(messageBox);
 
                 AudioClip clip = DaggerfallUnity.Instance.SoundReader.GetAudioClip((uint)selectedRace.ClipID);
-                DaggerfallUI.Instance.AudioSource.PlayOneShot(clip);
+                DaggerfallUI.Instance.AudioSource.PlayOneShot(clip, DaggerfallUnity.Settings.SoundVolume);
             }
         }
 

Assets/Scripts/Internal/DaggerfallAction.cs
@@ -248,7 +248,10 @@ namespace DaggerfallWorkshop
                 ActivateNext();
 
             if (PlaySound && Index > 0 && audioSource)
+            {
+                audioSource.volume = DaggerfallUnity.Settings.SoundVolume;
                 audioSource.Play();
+            }
 
             //stop if failed to get valid delegate from lookup - ideally this check should be done before playing
             //sound & activating next, but for testing purposes is done after

commit f2ffd84eef97496e811bb8ca38e05d732a43af7e

Fix spurious horse sound in TediousTravel mod
Bug introduced by 60d083e
See https://forums.dfworkshop.net/viewtopic.php?f=27&t=1293&p=17954#p17954

Assets/Scripts/Game/TransportManager.cs
@@ -155,7 +155,7 @@ namespace DaggerfallWorkshop.Game
 
                     if (!ridingAudioSource.isPlaying)
                     {
-                        ridingAudioSource.volume = DaggerfallUnity.Settings.SoundVolume;
+                        ridingAudioSource.volume = RidingVolumeScale * DaggerfallUnity.Settings.SoundVolume;
                         ridingAudioSource.Play();
                     }
                 }