f8dc2d9b705f5c25cc3651ddd0baeb59c0e9e513
Allofich <Allofich@users.noreply.github.com>
2018-12-09 18:44:54 +0900

    Don't show lock messages for AI

--- Assets/Scripts/Internal/DaggerfallActionDoor.cs
+++ Assets/Scripts/Internal/DaggerfallActionDoor.cs
@@ -1,4 +1,4 @@
-﻿// Project:         Daggerfall Tools For Unity
+// Project:         Daggerfall Tools For Unity
 // Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
 // Web Site:        http://www.dfworkshop.net
 // License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
@@ -280,7 +280,7 @@ namespace DaggerfallWorkshop
             // Do nothing if door cannot be opened right now
             if ((IsLocked && !ignoreLocks) || IsOpen)
             {
-                if(!IsOpen)
+                if(!IsOpen && activatedByPlayer)
                     LookAtLock();
                 return;
             }
