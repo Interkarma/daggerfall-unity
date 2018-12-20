4296a33c10deb9075a4360f3c2deb05ecff05b8e
Andy Blower <andyblower@gmail.com>
2018-12-20 20:32:21 +0000

    Remove redundant bits

--- Assets/Scripts/Game/MagicAndEffects/EntityEffectManager.cs
+++ Assets/Scripts/Game/MagicAndEffects/EntityEffectManager.cs
@@ -221,7 +221,7 @@ namespace DaggerfallWorkshop.Game.MagicAndEffects
                 if (InputManager.Instance.ActionStarted(InputManager.Actions.RecastSpell) && lastSpell != null &&
                     !GameManager.Instance.PlayerSpellCasting.IsPlayingAnim)
                 {
-                    if (GameManager.Instance.PlayerEntity.Items.Contains(Items.ItemGroups.MiscItems, (int)Items.MiscItems.Spellbook))
+                    if (GameManager.Instance.PlayerEntity.Items.Contains(ItemGroups.MiscItems, (int)MiscItems.Spellbook))
                         SetReadySpell(lastSpell);
                     else
                         DaggerfallUI.AddHUDText(TextManager.Instance.GetText(textDatabase, "noSpellbook"));
