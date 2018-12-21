a38c8cdde0b8d4ae9c0e296611a4c5164019d711
Andy Blower <andyblower@gmail.com>
2018-12-21 13:27:52 +0000

    Add use magic items list picker
    Using standard list picker window rather than implementing the small list from the old banner UI mode. I think this is better, and less work since it's reusing the existing component.

--- Assets/Scripts/Game/UserInterfaceWindows/DaggerfallPotionMakerWindow.cs
+++ Assets/Scripts/Game/UserInterfaceWindows/DaggerfallPotionMakerWindow.cs
@@ -54,7 +54,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
         #region UI Controls
 
         TextLabel nameLabel = new TextLabel();
-        TextLabel costLabel = new TextLabel();
+        //TextLabel costLabel = new TextLabel();
         TextLabel goldLabel = new TextLabel();
 
         Button recipesButton;
@@ -194,7 +194,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
         void SetupLabels()
         {
             nameLabel = DaggerfallUI.AddDefaultShadowedTextLabel(new Vector2(33, 185), NativePanel);
-            costLabel = DaggerfallUI.AddDefaultShadowedTextLabel(new Vector2(174, 185), NativePanel);
+            //costLabel = DaggerfallUI.AddDefaultShadowedTextLabel(new Vector2(174, 185), NativePanel);
             goldLabel = DaggerfallUI.AddDefaultShadowedTextLabel(new Vector2(237, 185), NativePanel);
         }
 
