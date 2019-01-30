3c2998bf0e41c099289477954c5db7d367b96eea
Lypyl <lypyldf@gmail.com>
2019-01-30 00:46:34 -0500

    Removed obsolete serialization process previously used by mod system

--- Assets/Game/Addons/ModSupport/ModTypes.cs
+++ Assets/Game/Addons/ModSupport/ModTypes.cs
@@ -114,29 +114,6 @@ namespace DaggerfallWorkshop.Game.Utility.ModSupport
         }
     }
 
-    [Serializable]
-    public struct SerializedRecord
-    {
-        public readonly string gameObjectName;
-        public readonly Type componentType;
-        public readonly object[] serializedObjects;
-
-        public SerializedRecord(string goName, Component compType, params object[] toSerialize)
-        {
-            this.gameObjectName = goName;
-            this.componentType = compType.GetType();
-            this.serializedObjects = toSerialize;
-        }
-    }
-
-    [Obsolete("Obsolete. Please use ImportedComponent attribute instead.")]
-    public interface Idfmod_Serializable
-    {
-        System.Object[] ToSerialize();
-        void Deseralized(params System.Object[] obj);
-        bool Ignore { get; set; }
-    }
-
     /// <summary>
     /// Takes part of load/save logic.
     /// </summary>
