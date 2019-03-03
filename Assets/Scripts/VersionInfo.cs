7fb125bfaa61505706700e84f8b010fe484492fc
Interkarma <interkarma@dfworkshop.net>
2019-03-03 11:50:49 +1000

    Bumping versions ahead of new builds

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -16,9 +16,9 @@ public static class VersionInfo
     public const string DaggerfallUnityProductName = "Daggerfall Unity";
     public const string DaggerfallUnityStatus = "pre-alpha";
 
-    // Last updated versions 30-January-2019
-    public const string DaggerfallUnityVersion = "0.7.36";
-    public const string DaggerfallToolsForUnityVersion = "1.8.1";
+    // Last updated versions 03-March-2019
+    public const string DaggerfallUnityVersion = "0.7.65";
+    public const string DaggerfallToolsForUnityVersion = "1.8.2";
     
     public const string RecommendedUnityVersion = "2018.2.11f1";
 }
