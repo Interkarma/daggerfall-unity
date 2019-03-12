ed0bdb56ae453fcc6cd654fba001d26fbc3f76ae
Interkarma <interkarma@dfworkshop.net>
2019-03-13 08:45:38 +1000

    Update copyright date range

--- Assets/Scripts/Game/Questing/Actions/EndQuest.cs
+++ Assets/Scripts/Game/Questing/Actions/EndQuest.cs
@@ -1,5 +1,5 @@
-﻿// Project:         Daggerfall Tools For Unity
-// Copyright:       Copyright (C) 2009-2018 Daggerfall Workshop
+// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2019 Daggerfall Workshop
 // Web Site:        http://www.dfworkshop.net
 // License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
 // Source Code:     https://github.com/Interkarma/daggerfall-unity
