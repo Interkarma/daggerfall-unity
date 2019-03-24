316454236486021bd31ab6c8e5f0164fdfa5202d
Interkarma <interkarma@dfworkshop.net>
2019-03-24 16:25:44 +1000

    Comment out duplicate language tally
    @allofich spotted that language skills were being tallied in both FormulaHelper and EnemySenses.
    Commented out additional tally in FormulaHelper as it seems most appropriate for CalculateEnemyPacification() just determine success/failure of pacification attempt. Now tally is performed at the same time as enemy is pacified.
    @ajrb Please let me know if you'd prefer this handled a different way. :)

--- Assets/Scripts/Game/Formulas/FormulaHelper.cs
+++ Assets/Scripts/Game/Formulas/FormulaHelper.cs
@@ -317,10 +317,10 @@ namespace DaggerfallWorkshop.Game.Formulas
 
             int roll = UnityEngine.Random.Range(0, 200);
             bool success = (roll < chance);
-            if (success)
-                player.TallySkill(languageSkill, 1);
-            else if (languageSkill != DFCareer.Skills.Etiquette && languageSkill != DFCareer.Skills.Streetwise)
-                player.TallySkill(languageSkill, 1);
+            //if (success)
+            //    player.TallySkill(languageSkill, 1);
+            //else if (languageSkill != DFCareer.Skills.Etiquette && languageSkill != DFCareer.Skills.Streetwise)
+            //    player.TallySkill(languageSkill, 1);
 
             Debug.LogFormat("Pacification {3} using {0} skill: chance= {1}  roll= {2}", languageSkill, chance, roll, success ? "success" : "failure");
             return success;
