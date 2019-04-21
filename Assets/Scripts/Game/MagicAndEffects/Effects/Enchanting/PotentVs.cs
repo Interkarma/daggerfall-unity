93fbd4f22bcacf39db0cf6c23755bf039c35636e
Interkarma <interkarma@dfworkshop.net>
2019-04-22 09:19:06 +1000

    Implement PotentVs enchantment settings

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Enchanting/PotentVs.cs
@@ -0,0 +1,78 @@
+// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2019 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using System.Collections.Generic;
+using DaggerfallConnect.FallExe;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Potent vs enemy types.
+    /// </summary>
+    public class PotentVs : BaseEntityEffect
+    {
+        public static readonly string EffectKey = EnchantmentTypes.PotentVs.ToString();
+
+        public override void SetProperties()
+        {
+            properties.Key = EffectKey;
+            properties.GroupName = TextManager.Instance.GetText(textDatabase, EffectKey);
+            properties.AllowedCraftingStations = MagicCraftingStations.ItemMaker;
+        }
+
+        /// <summary>
+        /// Outputs all variant settings for this enchantment.
+        /// </summary>
+        public override EnchantmentSettings[] GetEnchantmentSettings()
+        {
+            List<EnchantmentSettings> enchantments = new List<EnchantmentSettings>();
+
+            // Enumerate classic params
+            for (int i = 0; i < classicParamCosts.Length; i++)
+            {
+                EnchantmentSettings enchantment = new EnchantmentSettings()
+                {
+                    Version = 1,
+                    EffectKey = EffectKey,
+                    ClassicType = EnchantmentTypes.PotentVs,
+                    ClassicParam = (short)i,
+                    PrimaryDisplayName = properties.GroupName,
+                    SecondaryDisplayName = TextManager.Instance.GetText(textDatabase, classicTextKeys[i]),
+                    EnchantCost = classicParamCosts[i],
+                };
+
+                enchantments.Add(enchantment);
+            }
+
+            return enchantments.ToArray();
+        }
+
+        #region Classic Support
+
+        static short[] classicParamCosts =
+        {
+            800,    //Undead
+            900,    //Daedra
+            1000,   //Humanoid
+            1200,   //Animals
+        };
+
+        static string[] classicTextKeys =
+        {
+            "undead",
+            "daedra",
+            "humanoid",
+            "animalsUpper",
+        };
+
+        #endregion
+    }
+}
