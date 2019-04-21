2279595a84bf7caf7c1e633234b9f6f2c1babf60
Interkarma <interkarma@dfworkshop.net>
2019-04-22 09:23:41 +1000

    Implement RegensHealth enchantment settings

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Enchanting/RegensHealth.cs
@@ -0,0 +1,76 @@
+// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2019 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using System.Collections.Generic;
+using DaggerfallConnect.FallExe;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Regenerate health under specific conditions.
+    /// </summary>
+    public class RegensHealth : BaseEntityEffect
+    {
+        public static readonly string EffectKey = EnchantmentTypes.RegensHealth.ToString();
+
+        public override void SetProperties()
+        {
+            properties.Key = EffectKey;
+            properties.GroupName = TextManager.Instance.GetText(textDatabase, EffectKey);
+            properties.AllowedCraftingStations = MagicCraftingStations.ItemMaker;
+        }
+
+        /// <summary>
+        /// Outputs all variant settings for this enchantment.
+        /// </summary>
+        public override EnchantmentSettings[] GetEnchantmentSettings()
+        {
+            List<EnchantmentSettings> enchantments = new List<EnchantmentSettings>();
+
+            // Enumerate classic params
+            for (int i = 0; i < classicParamCosts.Length; i++)
+            {
+                EnchantmentSettings enchantment = new EnchantmentSettings()
+                {
+                    Version = 1,
+                    EffectKey = EffectKey,
+                    ClassicType = EnchantmentTypes.PotentVs,
+                    ClassicParam = (short)i,
+                    PrimaryDisplayName = properties.GroupName,
+                    SecondaryDisplayName = TextManager.Instance.GetText(textDatabase, classicTextKeys[i]),
+                    EnchantCost = classicParamCosts[i],
+                };
+
+                enchantments.Add(enchantment);
+            }
+
+            return enchantments.ToArray();
+        }
+
+        #region Classic Support
+
+        static short[] classicParamCosts =
+        {
+            4000,   //all the time
+            3000,   //in sunlight
+            3000,   //in darkness
+        };
+
+        static string[] classicTextKeys =
+        {
+            "allTheTime",
+            "inSunlight",
+            "inDarkness",
+        };
+
+        #endregion
+    }
+}
