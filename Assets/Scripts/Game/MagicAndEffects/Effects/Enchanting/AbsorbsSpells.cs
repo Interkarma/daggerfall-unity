763e8d3db09f20055d6f565c039b1225441c2930
Interkarma <interkarma@dfworkshop.net>
2019-04-22 10:25:11 +1000

    Implement AbsorbsSpells enchantment settings

---
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Enchanting/AbsorbsSpells.cs
@@ -0,0 +1,49 @@
+// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2019 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Gavin Clayton (interkarma@dfworkshop.net)
+// Contributors:    
+// 
+// Notes:
+//
+
+using DaggerfallConnect.FallExe;
+
+namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
+{
+    /// <summary>
+    /// Absorbs spells.
+    /// </summary>
+    public class AbsorbsSpells : BaseEntityEffect
+    {
+        public static readonly string EffectKey = EnchantmentTypes.AbsorbsSpells.ToString();
+
+        const int enchantCost = 1500;
+
+        public override void SetProperties()
+        {
+            properties.Key = EffectKey;
+            properties.GroupName = TextManager.Instance.GetText(textDatabase, EffectKey);
+            properties.AllowedCraftingStations = MagicCraftingStations.ItemMaker;
+            properties.ItemMakerFlags = ItemMakerFlags.AllowMultiplePrimaryInstances;
+        }
+
+        public override EnchantmentSettings[] GetEnchantmentSettings()
+        {
+            EnchantmentSettings[] enchantments = new EnchantmentSettings[1];
+            enchantments[0] = new EnchantmentSettings()
+            {
+                Version = 1,
+                EffectKey = EffectKey,
+                ClassicType = EnchantmentTypes.AbsorbsSpells,
+                ClassicParam = -1,
+                PrimaryDisplayName = properties.GroupName,
+                EnchantCost = enchantCost,
+            };
+
+            return enchantments;
+        }
+    }
+}
