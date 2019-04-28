Interkarma/daggerfall-unity

commit 94a2c78b3881664b5c58d37f059207e30cd11aaa

Address warnings
Make line endings consistent
Comment out unused variables
Use correct layer name in ExteriorAutomap

Assets/Scripts/Utility/RMBLayout.cs
@@ -812,10 +812,10 @@ namespace DaggerfallWorkshop.Utility
             go.transform.rotation = GameObjectHelper.QuaternionFromMatrix(matrix);
 
             // Is this a city gate?
-            if (IsCityGate(modelID))
+            /*if (IsCityGate(modelID))
             {
                 DaggerfallCityGate gate = go.AddComponent<DaggerfallCityGate>();
-            }
+            }*/
 
             return go;
         }

Assets/Scripts/Editor/DaggerfallUnityEditor.cs
@@ -166,7 +166,7 @@ namespace DaggerfallWorkshop
                 // Import options
                 var propAddMeshColliders = Prop("Option_AddMeshColliders");
                 var propRMBGroundPlane = Prop("Option_RMBGroundPlane");
-                var propCloseCityGates = Prop("Option_CloseCityGates");
+                //var propCloseCityGates = Prop("Option_CloseCityGates");
                 EditorGUILayout.Space();
                 EditorGUILayout.LabelField("Import Options");
                 GUILayoutHelper.Indent(() =>

Assets/Scripts/Game/Entities/PlayerEntity.cs
@@ -45,7 +45,7 @@ namespace DaggerfallWorkshop.Game.Entity
         bool preventNormalizingReputations = false;
         bool isResting = false;
 
-        bool hasStartedInitialVampireQuest = false;
+        //bool hasStartedInitialVampireQuest = false;
         //byte vampireClan = 0;
         //uint lastTimeVampireNeedToKillSatiated = 0;
 
@@ -805,7 +805,7 @@ namespace DaggerfallWorkshop.Game.Entity
             this.timeForDarkBrotherhoodLetter = character.timeForDarkBrotherhoodLetter;
             this.darkBrotherhoodRequirementTally = character.darkBrotherhoodRequirementTally;
             this.thievesGuildRequirementTally = character.thievesGuildRequirementTally;
-            this.hasStartedInitialVampireQuest = character.hasStartedInitialVampireQuest != 0;
+            //this.hasStartedInitialVampireQuest = character.hasStartedInitialVampireQuest != 0;
             //this.vampireClan = character.vampireClan;
             //this.lastTimeVampireNeedToKillSatiated = character.lastTimeVampireNeedToKillSatiated;
 

Assets/Scripts/Game/ExteriorAutomap.cs
@@ -439,7 +439,7 @@ namespace DaggerfallWorkshop.Game
                     Application.Quit();
             }
 
-            layerAutomap = LayerMask.NameToLayer("ExteriorAutomap");
+            layerAutomap = LayerMask.NameToLayer("Automap");
             if (layerAutomap == -1)
             {
                 DaggerfallUnity.LogMessage("Did not find Layer with name \"Automap\"! Defaulting to Layer 10\nIt is prefered that Layer \"Automap\" is set in Unity Editor under \"Edit/Project Settings/Tags and Layers!\"", true);

Assets/Scripts/Game/Player/ClimbingMotor.cs
@@ -12,7 +12,7 @@ namespace DaggerfallWorkshop.Game
     public class ClimbingMotor : MonoBehaviour
     {
         private Entity.PlayerEntity player;
-        private PlayerGroundMotor groundMotor;
+        //private PlayerGroundMotor groundMotor;
         private RappelMotor rappelMotor;
         private HangingMotor hangingMotor;
         private PlayerMotor playerMotor;
@@ -95,7 +95,7 @@ namespace DaggerfallWorkshop.Game
         {
             player = GameManager.Instance.PlayerEntity;
             playerMotor = GetComponent<PlayerMotor>();
-            groundMotor = GetComponent<PlayerGroundMotor>();
+            //groundMotor = GetComponent<PlayerGroundMotor>();
             levitateMotor = GetComponent<LevitateMotor>();
             controller = GetComponent<CharacterController>();
             playerEnterExit = GetComponent<PlayerEnterExit>();
@@ -151,7 +151,7 @@ namespace DaggerfallWorkshop.Game
             bool horizontallyStationary = Vector2.Distance(lastHorizontalPosition, new Vector2(controller.transform.position.x, controller.transform.position.z)) < startClimbHorizontalTolerance;
             bool touchingSides = (playerMotor.CollisionFlags & CollisionFlags.Sides) != 0;
             bool touchingGround = (playerMotor.CollisionFlags & CollisionFlags.Below) != 0;
-            bool touchingAbove = (playerMotor.CollisionFlags & CollisionFlags.Above) != 0;
+            //bool touchingAbove = (playerMotor.CollisionFlags & CollisionFlags.Above) != 0;
             bool slippedToGround = isSlipping && touchingGround;
             bool nonOrthogonalStart = !isClimbing && inputForward && !horizontallyStationary;
             bool forwardStationaryNearCeiling = inputForward && hangingMotor.IsWithinHangingDistance && horizontallyStationary;

Assets/Scripts/Game/Player/HangingMotor.cs
@@ -13,9 +13,9 @@ namespace DaggerfallWorkshop.Game
         private RappelMotor rappelMotor;
         private ClimbingMotor climbingMotor;
         private PlayerMoveScanner scanner;
-        private Transform camTransform;
+        //private Transform camTransform;
         private PlayerMotor playerMotor;
-        private Entity.PlayerEntity player;
+        //private Entity.PlayerEntity player;
 
         private enum HangingTransitionState
         {
@@ -28,7 +28,7 @@ namespace DaggerfallWorkshop.Game
         private float hangingContinueTimer = 0;
         private float rappelTimer;
         private bool isLosingGrip = false;
-        private Vector3 lastPosition = Vector3.zero;
+        //private Vector3 lastPosition = Vector3.zero;
         private Vector2 lastHorizontalPosition = Vector2.zero;
         private bool showHangingModeMessage = true;
         private float startHangingHorizontalTolerance = 0.12f;
@@ -48,7 +48,7 @@ namespace DaggerfallWorkshop.Game
         public bool IsWithinHangingDistance { get; private set; }
         private void Start()
         {
-            player = GameManager.Instance.PlayerEntity;
+            //player = GameManager.Instance.PlayerEntity;
             playerMotor = GetComponent<PlayerMotor>();
             acrobatMotor = GetComponent<AcrobatMotor>();
             levitateMotor = GetComponent<LevitateMotor>();
@@ -58,7 +58,7 @@ namespace DaggerfallWorkshop.Game
             rappelMotor = GetComponent<RappelMotor>();
             climbingMotor = GetComponent<ClimbingMotor>();
             scanner = GetComponent<PlayerMoveScanner>();
-            camTransform = GameManager.Instance.MainCamera.transform;
+            //camTransform = GameManager.Instance.MainCamera.transform;
         }
 
         public void HangingChecks()
@@ -70,10 +70,10 @@ namespace DaggerfallWorkshop.Game
 
             float continueHangingSkillCheckFrequency = 14f;
             float halfHeight = (controller.height / 2f);
-            bool inputBack = InputManager.Instance.HasAction(InputManager.Actions.MoveBackwards);
+            //bool inputBack = InputManager.Instance.HasAction(InputManager.Actions.MoveBackwards);
             bool inputForward = InputManager.Instance.HasAction(InputManager.Actions.MoveForwards);
 
-            bool touchingSides = (playerMotor.CollisionFlags & CollisionFlags.Sides) != 0;
+            //bool touchingSides = (playerMotor.CollisionFlags & CollisionFlags.Sides) != 0;
             bool touchingAbove = (playerMotor.CollisionFlags & CollisionFlags.Above) != 0;
             IsWithinHangingDistance = (scanner.HeadHitDistance > halfHeight - 0.17f && scanner.HeadHitDistance < halfHeight - 0.09f);
 

Assets/Scripts/Game/Player/PlayerMoveScanner.cs
@@ -72,7 +72,7 @@ namespace DaggerfallWorkshop.Game
         HangingMotor hangingMotor;
         ClimbingMotor climbingMotor;
         AcrobatMotor acrobatMotor;
-        PlayerMotor playerMotor;
+        //PlayerMotor playerMotor;
         private int turnCount = 0;
 
         public float HeadHitRadius { get; private set; }
@@ -108,7 +108,7 @@ namespace DaggerfallWorkshop.Game
         {
             controller = GetComponent<CharacterController>();
             acrobatMotor = GetComponent<AcrobatMotor>();
-            playerMotor = GetComponent<PlayerMotor>();
+            //playerMotor = GetComponent<PlayerMotor>();
             hangingMotor = GetComponent<HangingMotor>();
             climbingMotor = GetComponent<ClimbingMotor>();
             HeadHitRadius = controller.radius * 0.85f;

commit c882afac21cca8cadf6910ab8347c695646dcaae

fix city gates regression
Broken in 94a2c78b

Forums: https://forums.dfworkshop.net/viewtopic.php?f=24&t=2002

Assets/Scripts/Utility/RMBLayout.cs
@@ -812,10 +812,10 @@ namespace DaggerfallWorkshop.Utility
             go.transform.rotation = GameObjectHelper.QuaternionFromMatrix(matrix);
 
             // Is this a city gate?
-            /*if (IsCityGate(modelID))
+            if (IsCityGate(modelID))
             {
                 DaggerfallCityGate gate = go.AddComponent<DaggerfallCityGate>();
-            }*/
+            }
 
             return go;
         }