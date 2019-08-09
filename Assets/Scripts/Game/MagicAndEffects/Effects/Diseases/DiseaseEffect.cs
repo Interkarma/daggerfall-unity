7387df9ca290bfde657e393de919668a2a046c90
Interkarma <interkarma@dfworkshop.net>
2019-08-09 23:23:45 +1000

    Only player can contract Disease-based effects
    If a disease effect is transported to a non-player entity or player entity is less than level 2, then disease will end immediately and expire without payload.

--- Assets/Scripts/Game/MagicAndEffects/Effects/Diseases/DiseaseEffect.cs
+++ Assets/Scripts/Game/MagicAndEffects/Effects/Diseases/DiseaseEffect.cs
@@ -88,11 +88,11 @@ namespace DaggerfallWorkshop.Game.MagicAndEffects.MagicEffects
 
         public override void Start(EntityEffectManager manager, DaggerfallEntityBehaviour caster = null)
         {
-            // Player must be greater than level 1 to acquire a disease
+            // Target must be player entity greater than level 1 to acquire a disease
             DaggerfallEntityBehaviour host = GetPeeredEntityBehaviour(manager);
-            if (host.EntityType == EntityTypes.Player && host.Entity.Level < 2)
+            if (host.EntityType != EntityTypes.Player || host.Entity.Level < 2)
             {
-                forcedRoundsRemaining = 0;
+                EndDisease();
                 return;
             }
 
