f79759fb3921b13879656ecbbd66ddf91e8f1c12
Pierre Etchemaite <petchema@concept-micro.com>
2019-09-21 21:41:13 +0200

    lowering stuck threshold so it doesn't trigger while crouching

--- Assets/Scripts/Game/Player/FrictionMotor.cs
+++ Assets/Scripts/Game/Player/FrictionMotor.cs
@@ -129,7 +129,7 @@ namespace DaggerfallWorkshop.Game
             //  -Tolerances should prevent false positives, but it's still possible - might require tuning/reworking later
             //  -Works best when player is standing as spherecast test has more clearance
             //  -Enemies will still become stuck as their motor does not have this handling
-            const float stuckMovementThreshold = 0.25f;
+            const float stuckMovementThreshold = 0.07f;
             const float stuckSampleDistance = 0.5f;
             const int stuckFrameThreshold = 3;
             bool tryingToMoveForwards = InputManager.Instance.HasAction(InputManager.Actions.MoveForwards);
