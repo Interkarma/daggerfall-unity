182167c7fbc2003343a03b67db00a9b7455a84f8
Andy Blower <andyblower@gmail.com>
2019-11-08 21:20:44 +0000

    Fix a bug when entering certain buildings riding a horse
    Some extry doors are raised and cause issues when entered while on a horse since the controller height is larger so the raycast finds the streaming terrain and the player is then moved under the floor by PlayerEnterExit.SetStanding()
    This changes that method to always use the standing controller height rather than the dynamic value.
    Reproduce the bug by entering the palace at Issaqumbaa  in Abibon-Gora while riding. Before this fix you fall though the world.

--- Assets/Scripts/Game/PlayerEnterExit.cs
+++ Assets/Scripts/Game/PlayerEnterExit.cs
@@ -1160,7 +1160,7 @@ namespace DaggerfallWorkshop.Game
             // Snap player to ground
             RaycastHit hit;
             Ray ray = new Ray(transform.position, Vector3.down);
-            if (Physics.Raycast(ray, out hit, controller.height * 2f))
+            if (Physics.Raycast(ray, out hit, PlayerHeightChanger.controllerStandingHeight * 2f))
             {
                 // Clear falling damage so player doesn't take damage if they transitioned into a dungeon while jumping
                 GameManager.Instance.AcrobatMotor.ClearFallingDamage();
