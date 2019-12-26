3410b28509d0fef8462d9d5e0639958c51e808a7
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2019-12-25 20:50:29 -0600

    add npc health indicators mod

---
+++ Assets/Game/Mods/NPCHealthIndicators/Scripts/HealthIndicatorMod.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using UnityEngine;
+
+public class HealthIndicatorMod : MonoBehaviour
+{
+    private static Mod _mod;
+
+    private GameObject _npcHealthIndicatorPrefab;
+
+    [Invoke(StateManager.StateTypes.Start, 0)]
+    public static void Init(InitParams initParams)
+    {
+        _mod = initParams.Mod;
+        var gameObject = new GameObject(_mod.Title);
+        gameObject.AddComponent<HealthIndicatorMod>();
+    }
+
+    void Awake()
+    {
+        _npcHealthIndicatorPrefab = _mod.GetAsset<GameObject>("Assets/NPCHealthIndicator.prefab");
+        StartCoroutine(EntityBehavioursUpdateLoop());
+        _mod.IsReady = true;
+    }
+
+    private IEnumerator EntityBehavioursUpdateLoop()
+    {
+        while (true)
+        {
+            var entityBehaviours = FindObjectsOfType<DaggerfallEntityBehaviour>();
+            foreach (var entityBehaviour in entityBehaviours)
+            {
+                if (entityBehaviour.transform.CompareTag("Player"))
+                {
+                    continue;
+                }
+                var hasHealthIndicator = entityBehaviour.transform.Find("NPCHealthIndicator(Clone)") != null;
+                if (!hasHealthIndicator)
+                {
+                    var npcHealthIndicator = Instantiate(_npcHealthIndicatorPrefab, entityBehaviour.transform);
+                    npcHealthIndicator.AddComponent<NPCHealthIndicator>();
+                }
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
---
+++ Assets/Game/Mods/NPCHealthIndicators/Scripts/NPCHealthIndicator.cs
@@ -0,0 +1,36 @@
+﻿using DaggerfallWorkshop.Game.Entity;
+using TMPro;
+using UnityEngine;
+
+public class NPCHealthIndicator : MonoBehaviour
+{
+    private DaggerfallEntityBehaviour _entityBehaviour;
+    private TextMeshPro _textMeshPro;
+
+    private void Start()
+    {
+        _textMeshPro = gameObject.GetComponent<TextMeshPro>();
+        _entityBehaviour = GetComponentInParent<DaggerfallEntityBehaviour>();
+    }
+
+    private void Update()
+    {
+        UpdateHealthText();
+        UpdateRotation();
+    }
+
+    private void UpdateHealthText()
+    {
+        var healthPercent = _entityBehaviour.Entity.CurrentHealthPercent;
+        _textMeshPro.SetText(
+            healthPercent == 0 || healthPercent == 1
+                ? string.Empty
+                : string.Format("HP: {0:0}%", healthPercent * 100)
+        );
+    }
+
+    private void UpdateRotation()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
