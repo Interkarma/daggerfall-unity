810ae4e0be1d82d1bfb6f3f33354720fb17a31de
Interkarma <interkarma@dfworkshop.net>
2020-02-23 14:56:27 +1000

    Bump version to 0.10.20

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -16,8 +16,8 @@ public static class VersionInfo
     public const string DaggerfallUnityProductName = "Daggerfall Unity";
     public const string DaggerfallUnityStatus = "alpha";
 
-    // Last updated versions 08-February-2020
-    public const string DaggerfallUnityVersion = "0.10.19";
+    // Last updated versions 23-February-2020
+    public const string DaggerfallUnityVersion = "0.10.20";
     public const string DaggerfallToolsForUnityVersion = "1.8.7";
     
     public const string RecommendedUnityVersion = "2018.2.21f1";
