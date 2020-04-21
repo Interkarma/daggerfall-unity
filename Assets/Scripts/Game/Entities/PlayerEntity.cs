Interkarma/daggerfall-unity

commit 5c308738a2a863b9b2c4dfeed5c1c8d07391513f

Implement Foe item queues
When using "give item" to assign an item to a Foe resource, an clone of this item will now be added to each subsequent instance of that Foe. I've always thought this was a wonky mechanic in classic, but some quests actually rely on this to move Foes to different locations while retaining assigned items.
This also solves the issue of a quest item only being placed on on an enemy (e.g. when player enters dungeon) and losing it if player resets dungeon by leaving and entering again.
@jayh2971 Jay, could I please ask for some help testing this one? Could you run it through some quests and let me know if any problems or unexpected behaviours. This is a fairly complex change to item placement, but I think I have all the serialization and condition angles covered so that it should be stable. Thank you for your help!

Assets/Scripts/Game/Items/ItemCollection.cs
@@ -456,6 +456,40 @@ namespace DaggerfallWorkshop.Game.Items
         }
 
         /// <summary>
+        /// Adds items to this collection from an array of items.
+        /// Items in this collection will remain.
+        /// UIDs will be retained.
+        /// </summary>
+        /// <param name="items">Items array.</param>
+        public void AddItems(DaggerfallUnityItem[] items)
+        {
+            if (items == null || items.Length == 0)
+                return;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                AddItem(items[i]);
+            }
+        }
+
+        /// <summary>
+        /// Creates a clone of all items in this collection.
+        /// Items in this collection will remain.
+        /// Cloned items will have new UIDs.
+        /// </summary>
+        /// <returns>Array of cloned items.</returns>
+        public DaggerfallUnityItem[] CloneAll()
+        {
+            List<DaggerfallUnityItem> clonedItems = new List<DaggerfallUnityItem>();
+            foreach (DaggerfallUnityItem item in items.Values)
+            {
+                clonedItems.Add(item.Clone());
+            }
+
+            return clonedItems.ToArray();
+        }
+
+        /// <summary>
         /// Exports items from this collection to an array of items.
         /// Items in this collection will be destroyed.
         /// UIDs will be retained.

Assets/Scripts/Game/Entities/EnemyEntity.cs
@@ -33,6 +33,7 @@ namespace DaggerfallWorkshop.Game.Entity
         MobileEnemy mobileEnemy;
         bool pickpocketByPlayerAttempted = false;
         int questFoeSpellQueueIndex = -1;
+        int questFoeItemQueueIndex = -1;
 
         // From FALL.EXE offset 0x1C0F14
         static byte[] ImpSpells            = { 0x07, 0x0A, 0x1D, 0x2C };
@@ -79,6 +80,12 @@ namespace DaggerfallWorkshop.Game.Entity
         {
             get { return questFoeSpellQueueIndex; }
             set { questFoeSpellQueueIndex = value; }
+        }
+
+        public int QuestFoeItemQueueIndex
+        {
+            get { return questFoeItemQueueIndex; }
+            set { questFoeItemQueueIndex = value; }
         }
 
         public bool SoulTrapActive { get; set; }

Assets/Scripts/Game/Items/DaggerfallUnityItem.cs
@@ -492,7 +492,7 @@ namespace DaggerfallWorkshop.Game.Items
         /// <summary>
         /// Creates a new copy of this item.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Cloned item.</returns>
         public DaggerfallUnityItem Clone()
         {
             return new DaggerfallUnityItem(this);
@@ -1528,6 +1528,10 @@ namespace DaggerfallWorkshop.Game.Items
             message = other.message;
             potionRecipeKey = other.potionRecipeKey;
             timeHealthLeechLastUsed = other.timeHealthLeechLastUsed;
+
+            isQuestItem = other.isQuestItem;
+            questUID = other.questUID;
+            questItemSymbol = other.questItemSymbol;
 
             if (other.legacyMagic != null)
                 legacyMagic = (DaggerfallEnchantment[])other.legacyMagic.Clone();

Assets/Scripts/Game/Questing/Actions/GiveItem.cs
@@ -66,32 +66,52 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
                 throw new Exception(string.Format("Could not find target resource symbol {0}", targetSymbol));
             }
 
-            // Target must exist in the world
-            if (!target.QuestResourceBehaviour)
-                return;
-
-            // Must have an Entity to receive item
-            DaggerfallEntityBehaviour entityBehaviour = target.QuestResourceBehaviour.GetComponent<DaggerfallEntityBehaviour>();
-            if (!entityBehaviour)
+            // Add item to Foe resource or generic entity
+            // In practice GiveItem will always be to a Foe resource - NPCs are handled by "get item"
+            // Keeping generic GiveItem behaviour as entity catch-all and reference
+            if (target is Foe)
             {
-                SetComplete();
-                throw new Exception(string.Format("GiveItem target {0} is not an Entity with DaggerfallEntityBehaviour", targetSymbol));
-            }
+                // Add to Foe item queue
+                (target as Foe).QueueItem(item.DaggerfallUnityItem);
 
-            // Assign item for player to find
-            //  * Some quests assign item to Foe at create time, others on injured event
-            //  * It's possible for target enemy to be one-shot or to be killed by other means (such as "killall")
-            //  * This assignment will direct quest loot item either to live enemy or corpse loot container
-            if (entityBehaviour.CorpseLootContainer)
-            {
-                // If enemy is already dead then place item in corpse loot container
-                entityBehaviour.CorpseLootContainer.Items.AddItem(item.DaggerfallUnityItem);
+                // Dequeue items on entity immediately if target already exists in the world
+                // Will also handle placing items to dead enemy loot container
+                if (target.QuestResourceBehaviour)
+                {
+                    DaggerfallEntityBehaviour entityBehaviour = target.QuestResourceBehaviour.GetComponent<DaggerfallEntityBehaviour>();
+                    if (entityBehaviour)
+                        target.QuestResourceBehaviour.AddItemQueue(target as Foe, entityBehaviour);
+                }
             }
             else
             {
-                // Otherwise add quest Item to Entity item collection
-                // It will be transferred to corpse marker loot container when dropped
-                entityBehaviour.Entity.Items.AddItem(item.DaggerfallUnityItem);
+                // Target must exist in the world
+                if (!target.QuestResourceBehaviour)
+                    return;
+
+                // Must have an Entity to receive item
+                DaggerfallEntityBehaviour entityBehaviour = target.QuestResourceBehaviour.GetComponent<DaggerfallEntityBehaviour>();
+                if (!entityBehaviour)
+                {
+                    SetComplete();
+                    throw new Exception(string.Format("GiveItem target {0} is not an Entity with DaggerfallEntityBehaviour", targetSymbol));
+                }
+
+                // Assign item for player to find
+                //  * Some quests assign item to Foe at create time, others on injured event
+                //  * It's possible for target enemy to be one-shot or to be killed by other means (such as "killall")
+                //  * This assignment will direct quest loot item either to live enemy or corpse loot container
+                if (entityBehaviour.CorpseLootContainer)
+                {
+                    // If enemy is already dead then place item in corpse loot container
+                    entityBehaviour.CorpseLootContainer.Items.AddItem(item.DaggerfallUnityItem);
+                }
+                else
+                {
+                    // Otherwise add quest Item to Entity item collection
+                    // It will be transferred to corpse marker loot container when dropped
+                    entityBehaviour.Entity.Items.AddItem(item.DaggerfallUnityItem);
+                }
             }
 
             // Remove item from player inventory if they are holding it

Assets/Scripts/Game/Questing/Components/QuestResourceBehaviour.cs
@@ -16,6 +16,7 @@ using UnityEngine;
 using DaggerfallConnect.Save;
 using DaggerfallWorkshop.Game.Entity;
 using DaggerfallWorkshop.Game.MagicAndEffects;
+using DaggerfallWorkshop.Game.Items;
 using FullSerializer;
 
 namespace DaggerfallWorkshop.Game.Questing
@@ -33,6 +34,7 @@ namespace DaggerfallWorkshop.Game.Questing
         bool isFoeDead = false;
         bool restraintApplied = false;
         int foeSpellQueuePosition = 0;
+        int foeItemQueuePosition = 0;
 
         [NonSerialized] Quest targetQuest;
         [NonSerialized] QuestResource targetResource = null;
@@ -49,6 +51,7 @@ namespace DaggerfallWorkshop.Game.Questing
             public Symbol targetSymbol;
             public bool isFoeDead;
             public int foeSpellQueuePosition;
+            public int foeItemQueuePosition;
         }
 
         #endregion
@@ -153,8 +156,9 @@ namespace DaggerfallWorkshop.Game.Questing
                     return;
                 }
 
-                // Process spell queue
+                // Process spell and item queues
                 CastSpellQueue(foe, enemyEntityBehaviour);
+                AddItemQueue(foe, enemyEntityBehaviour);
 
                 // Handle restrained check
                 // This might need some tuning in relation to injured and death checks
@@ -252,6 +256,7 @@ namespace DaggerfallWorkshop.Game.Questing
             data.targetSymbol = targetSymbol;
             data.isFoeDead = isFoeDead;
             data.foeSpellQueuePosition = foeSpellQueuePosition;
+            data.foeItemQueuePosition = foeItemQueuePosition;
 
             return data;
         }
@@ -266,7 +271,97 @@ namespace DaggerfallWorkshop.Game.Questing
             targetSymbol = data.targetSymbol;
             isFoeDead = data.isFoeDead;
             foeSpellQueuePosition = data.foeSpellQueuePosition;
+            foeItemQueuePosition = data.foeItemQueuePosition;
             CacheTarget();
+        }
+
+        public void CastSpellQueue(Foe foe, DaggerfallEntityBehaviour enemyEntityBehaviour)
+        {
+            // Validate
+            if (!enemyEntityBehaviour || foe == null || foe.SpellQueue == null || foeSpellQueuePosition == foe.SpellQueue.Count)
+                return;
+
+            // Target entity must be alive
+            if (enemyEntityBehaviour.Entity.CurrentHealth == 0)
+                return;
+
+            // Get effect manager on enemy
+            EntityEffectManager enemyEffectManager = enemyEntityBehaviour.GetComponent<EntityEffectManager>();
+            if (!enemyEffectManager)
+                return;
+
+            // Cast queued spells on foe from current position
+            for (int i = foeSpellQueuePosition; i < foe.SpellQueue.Count; i++)
+            {
+                SpellReference spell = foe.SpellQueue[i];
+                EntityEffectBundle spellBundle = null;
+
+                // Create classic or custom spell bundle
+                if (string.IsNullOrEmpty(spell.CustomKey))
+                {
+                    // Get classic spell data
+                    SpellRecord.SpellRecordData spellData;
+                    if (!GameManager.Instance.EntityEffectBroker.GetClassicSpellRecord(spell.ClassicID, out spellData))
+                        continue;
+
+                    // Create classic spell bundle settings
+                    EffectBundleSettings bundleSettings;
+                    if (!GameManager.Instance.EntityEffectBroker.ClassicSpellRecordDataToEffectBundleSettings(spellData, BundleTypes.Spell, out bundleSettings))
+                        continue;
+
+                    // Create classic spell bundle
+                    spellBundle = new EntityEffectBundle(bundleSettings, enemyEntityBehaviour);
+                }
+                else
+                {
+                    // Create custom spell bundle - must be previously registered to broker
+                    try
+                    {
+                        EntityEffectBroker.CustomSpellBundleOffer offer = GameManager.Instance.EntityEffectBroker.GetCustomSpellBundleOffer(spell.CustomKey);
+                        spellBundle = new EntityEffectBundle(offer.BundleSetttings, enemyEntityBehaviour);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.LogErrorFormat("QuestResourceBehaviour.CastSpellQueue() could not find custom spell offer with key: {0}, exception: {1}", spell.CustomKey, ex.Message);
+                    }
+                }
+
+                // Assign spell bundle to enemy
+                if (spellBundle != null)
+                    enemyEffectManager.AssignBundle(spellBundle, AssignBundleFlags.BypassSavingThrows);
+            }
+
+            // Set index positon to end of queue
+            foeSpellQueuePosition = foe.SpellQueue.Count;
+        }
+
+        public void AddItemQueue(Foe foe, DaggerfallEntityBehaviour enemyEntityBehaviour)
+        {
+            // Validate
+            if (!enemyEntityBehaviour || foe == null || foe.ItemQueueCount == 0 || foeItemQueuePosition == foe.ItemQueueCount)
+                return;
+
+            // Get item queue as cloned items with new UIDs
+            DaggerfallUnityItem[] clonedItems = foe.GetClonedItemQueue();
+
+            // Assign all items for player to find
+            //  * Some quests assign item to Foe at create time, others on injured event
+            //  * It's possible for target enemy to be one-shot or to be killed by other means (such as "killall")
+            //  * This assignment will direct quest loot item either to live enemy or corpse loot container
+            if (enemyEntityBehaviour.CorpseLootContainer)
+            {
+                // If enemy is already dead then place item in corpse loot container
+                enemyEntityBehaviour.CorpseLootContainer.Items.AddItems(clonedItems);
+            }
+            else
+            {
+                // Otherwise add quest Item to Entity item collection
+                // It will be transferred to corpse marker loot container when dropped
+                enemyEntityBehaviour.Entity.Items.AddItems(clonedItems);
+            }
+
+            // Set index position to end of queue
+            foeItemQueuePosition = foe.ItemQueueCount;
         }
 
         #endregion
@@ -346,66 +441,6 @@ namespace DaggerfallWorkshop.Game.Questing
             }
 
             return matched;
-        }
-
-        void CastSpellQueue(Foe foe, DaggerfallEntityBehaviour enemyEntityBehaviour)
-        {
-            // Validate
-            if (!enemyEntityBehaviour || foe == null || foe.SpellQueue == null || foeSpellQueuePosition == foe.SpellQueue.Count)
-                return;
-
-            // Target entity must be alive
-            if (enemyEntityBehaviour.Entity.CurrentHealth == 0)
-                return;
-
-            // Get effect manager on enemy
-            EntityEffectManager enemyEffectManager = enemyEntityBehaviour.GetComponent<EntityEffectManager>();
-            if (!enemyEffectManager)
-                return;
-
-            // Cast queued spells on foe from current position
-            for (int i = foeSpellQueuePosition; i < foe.SpellQueue.Count; i++)
-            {
-                SpellReference spell = foe.SpellQueue[i];
-                EntityEffectBundle spellBundle = null;
-
-                // Create classic or custom spell bundle
-                if (string.IsNullOrEmpty(spell.CustomKey))
-                {
-                    // Get classic spell data
-                    SpellRecord.SpellRecordData spellData;
-                    if (!GameManager.Instance.EntityEffectBroker.GetClassicSpellRecord(spell.ClassicID, out spellData))
-                        continue;
-
-                    // Create classic spell bundle settings
-                    EffectBundleSettings bundleSettings;
-                    if (!GameManager.Instance.EntityEffectBroker.ClassicSpellRecordDataToEffectBundleSettings(spellData, BundleTypes.Spell, out bundleSettings))
-                        continue;
-
-                    // Create classic spell bundle
-                    spellBundle = new EntityEffectBundle(bundleSettings, enemyEntityBehaviour);
-                }
-                else
-                {
-                    // Create custom spell bundle - must be previously registered to broker
-                    try
-                    {
-                        EntityEffectBroker.CustomSpellBundleOffer offer = GameManager.Instance.EntityEffectBroker.GetCustomSpellBundleOffer(spell.CustomKey);
-                        spellBundle = new EntityEffectBundle(offer.BundleSetttings, enemyEntityBehaviour);
-                    }
-                    catch(Exception ex)
-                    {
-                        Debug.LogErrorFormat("QuestResourceBehaviour.CastSpellQueue() could not find custom spell offer with key: {0}, exception: {1}", spell.CustomKey, ex.Message);
-                    }
-                }
-
-                // Assign spell bundle to enemy
-                if (spellBundle != null)
-                    enemyEffectManager.AssignBundle(spellBundle, AssignBundleFlags.BypassSavingThrows);
-            }
-
-            // Set index positon to end of queue
-            foeSpellQueuePosition = foe.SpellQueue.Count;
         }
 
         #endregion

commit 06afcccec91eef98897ab00bea5fe277677f6c47

Fix regression caused by Foe item queues
Foe item queues broke certain items being removed correctly by other quest actions. This regression was introduced by commit https://github.com/Interkarma/daggerfall-unity/commit/5c308738a2a863b9b2c4dfeed5c1c8d07391513f
Problem is caused by item cloning behaviour of queues desyncing prototype item UID from cloned item UID. When ReleaseQuestItemForReoffer() was called by various quest actions, it was unable to find cloned items because it was searching on prototype item UID only.
Refactored ReleaseQuestItemForReoffer() to search on quest UID and item Symbol instead. This is more stable than item UID and will not break by cloning.
This fixes item removal behaviour for several quest actions broken by Foe item queues:
-GetItem
-GivePc
-TakeItem
-TotingItemAndClickedNpc
These actions will no longer leave behind orphaned items if previously cloned via Foe item queue. Process remains compatible with prototype items added directly to an item collection without first being cloned by Foe item queue.
Removed CleanupPlayerQuestItems() mitigation in Quest as it's no longer required now these quest actions have correct behaviour again.
Global orphaned item cleanup remains at load time.

Assets/Scripts/Game/Entities/PlayerEntity.cs
@@ -1484,26 +1484,32 @@ namespace DaggerfallWorkshop.Game.Entity
         /// Releases a quest item carried by player so it can be assigned back again by quest script.
         /// This ensures item is properly unequipped and optionally makes permanent.
         /// </summary>
-        /// <param name="item">Item to release.</param>
+        /// <param name="questUID">Quest UID owning Item.</param>
+        /// <param name="item">Quest Item to release.</param>
         /// <param name="makePermanent">True to make item permanent.</param>
-        public void ReleaseQuestItemForReoffer(DaggerfallUnityItem item, bool makePermanent = false)
+        public void ReleaseQuestItemForReoffer(ulong questUID, Item item, bool makePermanent = false)
         {
-            if (item == null)
+            if (item == null || item.Symbol == null)
                 return;
 
-            // Unequip item if player is wearing it
-            if (GameManager.Instance.PlayerEntity.ItemEquipTable.UnequipItem(item))
+            // Get all player held quest items matching this quest and item symbol
+            DaggerfallUnityItem[] items = GameManager.Instance.PlayerEntity.Items.ExportQuestItems(questUID, item.Symbol);
+            foreach (DaggerfallUnityItem dfitem in items)
             {
-                // If item was actually unequipped then update armour values
-                GameManager.Instance.PlayerEntity.UpdateEquippedArmorValues(item, false);
+                // Unequip item if player is wearing it
+                if (GameManager.Instance.PlayerEntity.ItemEquipTable.UnequipItem(dfitem))
+                {
+                    // If item was actually unequipped then update armour values
+                    GameManager.Instance.PlayerEntity.UpdateEquippedArmorValues(dfitem, false);
+                }
+
+                // Remove quest from inventory so it can be offered back to player
+                GameManager.Instance.PlayerEntity.Items.RemoveItem(dfitem);
+
+                // Optionally make permanent
+                if (makePermanent)
+                    dfitem.MakePermanent();
             }
-
-            // Remove quest from inventory so it can be offered back to player
-            GameManager.Instance.PlayerEntity.Items.RemoveItem(item);
-
-            // Optionally make permanent
-            if (makePermanent)
-                item.MakePermanent();
         }
 
         public void ClearReactionMods()

Assets/Scripts/Game/Items/ItemCollection.cs
@@ -510,16 +510,20 @@ namespace DaggerfallWorkshop.Game.Items
         }
 
         /// <summary>
-        /// Gets all quest items for a specific quest.
+        /// Gets all quest items for a specific quest and item Symbol.
         /// Ignores ex-quest items that have been made permanent.
         /// </summary>
         /// <param name="questUID">Quest UID for item search.</param>
-        public DaggerfallUnityItem[] ExportQuestItems(ulong questUID)
+        /// <param name="itemSymbol">Item Symbol for item search.</param>
+        public DaggerfallUnityItem[] ExportQuestItems(ulong questUID, Symbol itemSymbol)
         {
+            if (itemSymbol == null)
+                return null;
+
             List<DaggerfallUnityItem> results = new List<DaggerfallUnityItem>();
             foreach (DaggerfallUnityItem item in items.Values)
             {
-                if (item.IsQuestItem && item.QuestUID == questUID)
+                if (item.IsQuestItem && item.QuestUID == questUID && itemSymbol.Equals(item.QuestItemSymbol))
                     results.Add(item);
             }
 

Assets/Scripts/Game/Questing/Actions/GetItem.cs
@@ -67,7 +67,7 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
             // Release item so we can give back to player
             // Sometimes a quest item is both carried by player then handed back to them
             // Example is Sx012 where courier hands back two items of jewellery
-            GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(item.DaggerfallUnityItem);
+            GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(ParentQuest.UID, item);
 
             // Give quest item to player
             if (item.DaggerfallUnityItem.IsOfTemplate(ItemGroups.Currency, (int)Currency.Gold_pieces))

Assets/Scripts/Game/Questing/Actions/GivePc.cs
@@ -161,7 +161,7 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
             // Release item so we can offer back to player
             // Sometimes a quest item is both carried by player then offered back to them
             // Example is Sx010 where "curse" is removed and player can keep item
-            GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(item.DaggerfallUnityItem, true);
+            GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(ParentQuest.UID, item, true);
 
             // Create a dropped loot container window for player to loot their reward
             rewardLoot = GameObjectHelper.CreateDroppedLootContainer(GameManager.Instance.PlayerObject, DaggerfallUnity.NextUID);

Assets/Scripts/Game/Questing/Actions/TakeItem.cs
@@ -1,4 +1,4 @@
-﻿// Project:         Daggerfall Tools For Unity
+// Project:         Daggerfall Tools For Unity
 // Copyright:       Copyright (C) 2009-2020 Daggerfall Workshop
 // Web Site:        http://www.dfworkshop.net
 // License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
@@ -63,7 +63,7 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
 
             // Release item - this will clear equip state and remove item from player's inventory
             // Now item is not associated with any collections and will just be garbage collected
-            GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(item.DaggerfallUnityItem);
+            GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(ParentQuest.UID, item);
 
             // "saying" popup
             if (textId != 0)

Assets/Scripts/Game/Questing/Actions/TotingItemAndClickedNpc.cs
@@ -91,7 +91,7 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
 
                     // Show message popup, remove item, return true on trigger
                     ParentQuest.ShowMessagePopup(id);
-                    GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(item.DaggerfallUnityItem);
+                    GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(ParentQuest.UID, item);
                     return true;
                 }
             }

Assets/Scripts/Game/Questing/Quest.cs
@@ -285,10 +285,7 @@ namespace DaggerfallWorkshop.Game.Questing
             if (ticksToEnd > 0)
             {
                 if (--ticksToEnd == 0)
-                {
                     questComplete = true;
-                    CleanupPlayerQuestItems();
-                }
             }
 
             // Tick resources
@@ -521,30 +518,6 @@ namespace DaggerfallWorkshop.Game.Questing
             foreach(string key in keys)
             {
                 DropQuestor(new Symbol(key));
-            }
-        }
-
-        /// <summary>
-        /// Releases all orphaned non-permanent quest items from player inventory when quest is ended.
-        /// Item will no longer belong to any collection and will be garbage collected.
-        /// Only call this method once quest has fully completed and entered tombstone state.
-        /// </summary>
-        void CleanupPlayerQuestItems()
-        {
-            // Quest must be completed
-            if (!questComplete)
-                throw new Exception("CleanupPlayerQuestItems() called prior to quest completion.");
-
-            // Get all non-permanent quest items in player inventory linked to this quest UID
-            DaggerfallUnityItem[] items = GameManager.Instance.PlayerEntity.Items.ExportQuestItems(UID);
-            if (items == null || items.Length == 0)
-                return;
-
-            // Release these items from collection
-            // Using ReleaseQuestItemForReoffer() to ensure item is correctly unequipped before release as player may have equipped quest item
-            foreach (DaggerfallUnityItem item in items)
-            {
-                GameManager.Instance.PlayerEntity.ReleaseQuestItemForReoffer(item);
             }
         }
 