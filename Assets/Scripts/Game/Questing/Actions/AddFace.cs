03f8e291fd7ace43b77d555e6f59d3418974e785
Interkarma <interkarma@dfworkshop.net>
2020-05-22 09:40:42 +1000

    Dont rearm AddFace
    This action can spam portraits to HUD if rearmed multiple times in a row by switching parent task off then back on again. Action should only be executed once when called.

--- Assets/Scripts/Game/Questing/Actions/AddFace.cs
+++ Assets/Scripts/Game/Questing/Actions/AddFace.cs
@@ -36,6 +36,7 @@ namespace DaggerfallWorkshop.Game.Questing.Actions
         public AddFace(Quest parentQuest)
             : base(parentQuest)
         {
+            allowRearm = false;
         }
 
         public override IQuestAction CreateNew(string source, Quest parentQuest)
