Interkarma/daggerfall-unity

commit 801368dc3926f7c1ae65612bec08e232d354c652

squash

Assets/Scripts/Game/DaggerfallUI.cs
@@ -82,6 +82,7 @@ namespace DaggerfallWorkshop.Game
         DaggerfallFont[] daggerfallFonts = new DaggerfallFont[5];
         char lastCharacterTyped;
         KeyCode lastKeyCode;
+        bool processHotkeys;
         HotkeySequence.KeyModifiers lastKeyModifiers;
         bool hotkeySequenceProcessed = false;
         FadeBehaviour fadeBehaviour = null;
@@ -176,6 +177,8 @@ namespace DaggerfallWorkshop.Game
             get { return globalFilterMode; }
             set { globalFilterMode = value; }
         }
+
+        public Event KeyEvent { get; private set; } = new Event();
 
         public char LastCharacterTyped
         {
@@ -380,17 +383,13 @@ namespace DaggerfallWorkshop.Game
                 if (Event.current.character != (char)0)
                     lastCharacterTyped = Event.current.character;
 
-                if (Event.current.keyCode != KeyCode.None)
-                    lastKeyCode = Event.current.keyCode;
-
                 if (lastCharacterTyped > 255)
                     lastCharacterTyped = (char)0;
-
-                hotkeySequenceProcessed = ProcessHotKeySequences();
             }
 
-            if (Event.current.type == EventType.KeyUp)
+            if (processHotkeys)
             {
+                processHotkeys = false;
                 hotkeySequenceProcessed = ProcessHotKeySequences();
             }
 
@@ -415,6 +414,16 @@ namespace DaggerfallWorkshop.Game
                     RenderTexture.active = oldRT;
                 }
             }
+        }
+
+        public void OnKeyPress(KeyCode key, bool down)
+        {
+            KeyEvent.type = down ? EventType.KeyDown : EventType.KeyUp;
+            KeyEvent.keyCode = key;
+
+            if (down && key != KeyCode.None)
+                lastKeyCode = key;
+            processHotkeys = true;
         }
 
         private void InstantiatePersistentWindowInstances()

Assets/Scripts/Game/ControlsConfigManager.cs
@@ -147,14 +147,75 @@ namespace DaggerfallWorkshop.Game
         {
             HashSet<String> recorded = new HashSet<String>();
             HashSet<String> dupes = new HashSet<String>();
+            Dictionary<String, HashSet<String>> modifiers = new Dictionary<string, HashSet<string>>();
             String none = KeyCode.None.ToString();
 
-            foreach (String str in texts)
+            // Sort by combos first to record their modifiers
+            var list = texts.OrderByDescending(str =>
+            {
+                if (str.Contains('+'))
+                {
+                    var c = InputManager.Instance.GetCombo(InputManager.Instance.GetComboCode(str));
+                    // Only record the modifier (i.e. the button that gets held down first)
+                    var kc1 = InputManager.Instance.GetKeyString(c.Item1);
+
+                    // Add modifier to 'recorded' so it cannot be used as an independent keybind
+                    if (!recorded.Contains(kc1))
+                        recorded.Add(kc1);
+
+                    HashSet<String> mods;
+                    if (!modifiers.TryGetValue(kc1, out mods))
+                        mods = (modifiers[kc1] = new HashSet<String>());
+
+                    // Add combo to mod list
+                    mods.Add(str);
+
+                    return true;
+                }
+
+                return false;
+            });
+
+            // Simple check for duplicates in the list
+            // Example (both marked as dupes):
+            // "Action1: T"
+            // "Action2: T"
+            foreach (String str in list)
             {
                 if (!recorded.Contains(str))
                     recorded.Add(str);
                 else if (str != none)
                     dupes.Add(str);
+            }
+
+            // Mark combos as dupes too if a modifier has been used as an independent keybind
+            // Example (both marked as dupes):
+            // "Action1: LeftShift + T"
+            // "Action2: LeftShift"
+            foreach (var kv in modifiers)
+            {
+                if (dupes.Contains(kv.Key))
+                    dupes.UnionWith(kv.Value);
+            }
+
+            // Mark combos as dupes if the combo'd key is also used as a modifier
+            // Example (both marked as dupes):
+            // "Action1: LeftShift + T"
+            // "Action2: Z + LeftShift"
+            foreach (String str in list)
+            {
+                if (str.Contains('+'))
+                {
+                    var c = InputManager.Instance.GetCombo(InputManager.Instance.GetComboCode(str));
+                    var kc2 = InputManager.Instance.GetKeyString(c.Item2);
+                    HashSet<string> mods;
+
+                    if (modifiers.TryGetValue(kc2, out mods))
+                    {
+                        dupes.UnionWith(mods);
+                        dupes.Add(str);
+                    }
+                }
             }
             return dupes;
         }
@@ -439,18 +500,18 @@ namespace DaggerfallWorkshop.Game
             {
                 text = "Joy B" + (((int)key - 10) % 20);
             }
-            else if ((int)key >= InputManager.startingAxisKeyCode)
+            else if ((int)key >= InputManager.startingAxisKeyCode && (int)key < InputManager.startingComboKeyCode)
             {
                 text = "Joy" + (((int)key % InputManager.startingAxisKeyCode) / 2 + 1) + " B" + ((int)key % 2);
             }
+            else if ((int)key >= InputManager.startingComboKeyCode)
+            {
+                var combo = InputManager.Instance.GetCombo(key);
+                text = FormatButtonText(GetButtonText(combo.Item1) + " + " + GetButtonText(combo.Item2), fullString);
+            }
             else if (string.IsNullOrEmpty(text))
             {
-                var str = InputManager.Instance.GetKeyString((KeyCode)key);
-                if (str.Length <= maxButtonTextLength || fullString)
-                    //Split camel/pascal case by spaces
-                    text = Regex.Replace(str, "(?<=[a-z])([A-Z])", " $1", RegexOptions.Compiled).Trim();
-                else
-                    text = ElongatedButtonText;
+                text = FormatButtonText(InputManager.Instance.GetKeyString((KeyCode)key), fullString);
             }
 
             return DaggerfallUnity.Settings.SDFFontRendering ? text : text.ToUpper();
@@ -488,6 +549,15 @@ namespace DaggerfallWorkshop.Game
                     Debug.LogFormat("({0}) Bound Action {1} with Code {2} ({3})", primary ? "Primary" : "Secondary", action, code.ToString(), (int)code);
                 }
             }
+        }
+
+        private string FormatButtonText(string str, bool fullString)
+        {
+            if (str.Length <= maxButtonTextLength || fullString)
+                //Split camel/pascal case by spaces
+                return Regex.Replace(str, "(?<=[a-z])([A-Z])", " $1", RegexOptions.Compiled).Trim();
+
+            return ElongatedButtonText;
         }
 
         #endregion

Assets/Scripts/Game/UserInterface/Button.cs
@@ -78,11 +78,11 @@ namespace DaggerfallWorkshop.Game.UserInterface
 
         new public bool ProcessHotkeySequences(HotkeySequence.KeyModifiers keyModifiers)
         {
-            bool isKeyDown = Event.current.type == EventType.KeyDown;
+            bool isKeyDown = DaggerfallUI.Instance.KeyEvent.type == EventType.KeyDown;
             bool isActivated = isKeyDown ? shortcutKey.IsDownWith(keyModifiers) : shortcutKey.IsUpWith(keyModifiers);
             if (isActivated)
             {
-                if (!KeyboardEvent(Event.current))
+                if (!KeyboardEvent(DaggerfallUI.Instance.KeyEvent))
                 {
                     // Legacy support fallback, OnMouseClick handlers receive KeyDown events as faked clicks
                     if (isKeyDown)

Assets/Scripts/Game/UserInterfaceWindows/DaggerfallControlsWindow.cs
@@ -392,27 +392,38 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
                 setWaitingForInput(true);
                 yield return null;
             }
+
+            KeyCode code1 = InputManager.Instance.LastSingleKeyDown;
+
+            yield return new WaitForSecondsRealtime(0.05f);
+
+            while (!InputManager.Instance.AnyKeyDown)
+            {
+                if (InputManager.Instance.AnyKeyUp)
+                    break;
+
+                setWaitingForInput(true);
+                yield return null;
+            }
+
             setWaitingForInput(false);
 
-            KeyCode code = InputManager.Instance.LastKeyDown;
+            KeyCode code2 = InputManager.Instance.LastSingleKeyDown;
+            KeyCode code = code1 == code2 ? code1 : InputManager.Instance.GetComboCode(code1, code2);
 
-            if (code != KeyCode.None)
+            if (code != KeyCode.None && InputManager.Instance.ReservedKeys.FirstOrDefault(x => x == code) == KeyCode.None)
             {
-                if(InputManager.Instance.ReservedKeys.FirstOrDefault(x => x == code) == KeyCode.None)
-                {
-                    button.Label.Text = ControlsConfigManager.Instance.GetButtonText(code);
-                    button.SuppressToolTip = button.Label.Text != ControlsConfigManager.ElongatedButtonText;
-                    button.ToolTipText = ControlsConfigManager.Instance.GetButtonText(code, true);
+                button.Label.Text = ControlsConfigManager.Instance.GetButtonText(code);
+                button.SuppressToolTip = button.Label.Text != ControlsConfigManager.ElongatedButtonText;
+                button.ToolTipText = ControlsConfigManager.Instance.GetButtonText(code, true);
 
-                    var action = (InputManager.Actions)Enum.Parse(typeof(InputManager.Actions), button.Name);
-
-                    ControlsConfigManager.Instance.SetUnsavedBinding(action, InputManager.Instance.GetKeyString(code));
-                    checkDuplicates();
-                }
-                else
-                {
-                    button.Label.Text = currentLabel;
-                }
+                var action = (InputManager.Actions)Enum.Parse(typeof(InputManager.Actions), button.Name);
+                ControlsConfigManager.Instance.SetUnsavedBinding(action, InputManager.Instance.GetKeyString(code));
+                checkDuplicates();
+            }
+            else
+            {
+                button.Label.Text = currentLabel;
             }
         }
 

Assets/Scripts/Game/UserInterfaceWindows/DaggerfallJoystickControlsWindow.cs
@@ -542,7 +542,7 @@ namespace DaggerfallWorkshop.Game.UserInterfaceWindows
 
             SetWaitingForInput(false);
 
-            KeyCode code = InputManager.Instance.LastKeyDown;
+            KeyCode code = InputManager.Instance.LastSingleKeyDown;
 
             if (code != KeyCode.None)
             {

commit d0f604e62f581332cd6013fb714843dc6258a311

Store lastKeyCode during EventType.KeyDown handling
Makes key repeat events in UI available to TextBox input, e.g. Backspace, Delete and Arrow inputs.
Fixes #1971.
@jefetienne This was removed in commit 801368dc, which broke key-repeat for input fields. Can you recall why this was removed, and if you can think of any issues adding this back again? I haven't been able to determine any issues in game after adding this back, but I might not be considering something you're aware of.

Assets/Scripts/Game/DaggerfallUI.cs
@@ -384,6 +384,9 @@ namespace DaggerfallWorkshop.Game
                 if (Event.current.character != (char)0)
                     lastCharacterTyped = Event.current.character;
 
+                if (Event.current.keyCode != KeyCode.None)
+                    lastKeyCode = Event.current.keyCode;
+
                 if (lastCharacterTyped > 255)
                     lastCharacterTyped = (char)0;
             }