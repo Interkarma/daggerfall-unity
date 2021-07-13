dd9e021c127e358c6094b9e82e4095ad97f7008a
jefetienne <etienneayoub@gmail.com>
2021-07-12 22:10:55 -0400

    Move adding removedPrimaryAction to the part where serialization actually occurs

--- Assets/Scripts/Game/ControlsConfigManager.cs
+++ Assets/Scripts/Game/ControlsConfigManager.cs
@@ -311,9 +311,6 @@ namespace DaggerfallWorkshop.Game
                     button.Label.Text = KeyCode.None.ToString();
                     SetUnsavedBinding(action, button.Label.Text);
 
-                    if (UsingPrimary)
-                        InputManager.Instance.AddRemovedPrimaryAction(action);
-
                     checkDuplicates();
                 }
                 s.CloseWindow();
@@ -552,6 +549,9 @@ namespace DaggerfallWorkshop.Game
                 KeyCode curCode = InputManager.Instance.GetBinding(action, primary);
                 if (curCode != code)
                 {
+                    if (primary && code == KeyCode.None)
+                        InputManager.Instance.AddRemovedPrimaryAction(action);
+
                     InputManager.Instance.SetBinding(code, action, primary);
                     Debug.LogFormat("({0}) Bound Action {1} with Code {2} ({3})", primary ? "Primary" : "Secondary", action, code.ToString(), (int)code);
                 }
