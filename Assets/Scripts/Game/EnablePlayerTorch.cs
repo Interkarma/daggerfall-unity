e7f49619cf79b369dde6abc9d635643f28c6df03
Interkarma <interkarma@dfworkshop.net>
2021-10-14 12:51:05 +1000

    Scale item based torches and separate intensity settings
    A small revision of closed PR #2260.
    On Torch gameobject Light component, set intensity to flat 1.0 and range back to 6. This will now remain fixed.
    intensityMod is given a value of 0.75 (defaultTorchIntensity) or 1.5 (itemBasedTorchIntensity) depending on PlayerTorchFromItems pathway.
    Runtime torch intensity can be further tuned based on defaultTorchIntensity or itemBasedTorchIntensity respectively.
    I will look at tuning defaultTorchIntensity later and will leave item-based torch tuning to Hazelnut's preference.
    @ajrb I hope this helps. I know overall brightness of item based torch is a bit brighter than your original PR. But at least now we can tune each torch pathway separately without affecting the other. If you'd like to tune your item-based lights some more, please don't hesitate to send me your adjustments in another PR. Thanks for your help as always. :)

--- Assets/Scripts/Game/EnablePlayerTorch.cs
+++ Assets/Scripts/Game/EnablePlayerTorch.cs
@@ -28,7 +28,8 @@ namespace DaggerfallWorkshop.Game
         PlayerEntity playerEntity;
         Light torchLight;
         float torchIntensity;
-        float intensityMod = 1f;
+        float defaultTorchIntensity = 0.75f;
+        float itemBasedTorchIntensity = 1.5f;
         float tickTimeBuffer = 0f;
         float tickTimeInterval = 20f;
         float guttering = 0;
@@ -60,6 +61,7 @@ namespace DaggerfallWorkshop.Game
                 return;
 
             bool enableTorch = false;
+            float intensityMod = defaultTorchIntensity;
             if (DaggerfallUnity.Settings.PlayerTorchFromItems)
             {
                 DaggerfallUnityItem lightSource = playerEntity.LightSource;
@@ -88,12 +90,12 @@ namespace DaggerfallWorkshop.Game
                     if (lightSource.currentCondition < 3)
                     {
                         // Give warning signs if running low of fuel
-                        intensityMod = 0.6f + (Mathf.Cos(guttering) * 0.2f);
+                        intensityMod = 1f + (Mathf.Cos(guttering) * 0.2f);
                         guttering += Random.Range(-0.02f, 0.06f);
                     }
                     else
                     {
-                        intensityMod = 1;
+                        intensityMod = itemBasedTorchIntensity;
                         guttering = 0;
                     }
                 }
