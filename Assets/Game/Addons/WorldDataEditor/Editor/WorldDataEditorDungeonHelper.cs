2dc169442c8d405fc66ace923313aec355e863d4
Andy Blower <andyblower@gmail.com>
2021-11-22 21:19:39 +0000

    Add standard file headers

--- Assets/Game/Addons/WorldDataEditor/Editor/WorldDataEditorDungeonHelper.cs
+++ Assets/Game/Addons/WorldDataEditor/Editor/WorldDataEditorDungeonHelper.cs
@@ -1,3 +1,11 @@
+// Project:         Daggerfall Tools For Unity
+// Copyright:       Copyright (C) 2009-2021 Daggerfall Workshop
+// Web Site:        http://www.dfworkshop.net
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/Interkarma/daggerfall-unity
+// Original Author: Hazelnut
+// Contributors:    
+
 using System.IO;
 using UnityEngine;
 using DaggerfallWorkshop.Utility.AssetInjection;
