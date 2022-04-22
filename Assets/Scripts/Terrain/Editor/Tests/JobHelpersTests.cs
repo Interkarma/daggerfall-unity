54305dcb6d44eef71a9aaf1dc894b5b2f7d3c7c9
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2022-04-22 15:24:38 +0200

    added: JobHelpersTests.cs

---
+++ Assets/Scripts/Terrain/Editor/Tests/JobHelpersTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+namespace DaggerfallWorkshop.Tests
+{
+    static class JobATests
+    {
+        [Test] public static void JobA_Col___0_7() => Assert.AreEqual(expected: 0, actual: JobA.Col(0, 7));
+        [Test] public static void JobA_Col___1_7() => Assert.AreEqual(expected: 0, actual: JobA.Col(1, 7));
+        [Test] public static void JobA_Col___7_1() => Assert.AreEqual(expected: 7, actual: JobA.Col(7, 1));
+        [Test] public static void JobA_Col___7_7() => Assert.AreEqual(expected: 1, actual: JobA.Col(7, 7));
+
+        // [Test] public static void JobA_Row___0_7() => Assert.AreEqual(expected: ?, actual: JobA.Row(0, 7));
+        // [Test] public static void JobA_Row___1_7() => Assert.AreEqual(expected: ?, actual: JobA.Row(1, 7));
+        // [Test] public static void JobA_Row___7_1() => Assert.AreEqual(expected: ?, actual: JobA.Row(7, 1));
+        // [Test] public static void JobA_Row___7_7() => Assert.AreEqual(expected: ?, actual: JobA.Row(7, 7));
+    }
+}
