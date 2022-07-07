a8217747799ec793e2b9eaa975d1e6fc674428e4
Interkarma <interkarma@dfworkshop.net>
2022-07-07 13:10:31 +1000

    Update build date ahead of new builds

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -16,7 +16,7 @@ public static class VersionInfo
     public const string DaggerfallUnityProductName = "Daggerfall Unity";
     public const string DaggerfallUnityStatus = "beta";
 
-    // Last updated versions 05-July-2022
+    // Last updated versions 07-July-2022
     public const string DaggerfallUnityVersion = "0.14.1";
     public const string DaggerfallToolsForUnityVersion = "1.9.1";
     
