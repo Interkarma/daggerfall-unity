2ddab4e762a5193b13adeca827788bd1837979a5
Interkarma <interkarma@dfworkshop.net>
2022-07-19 09:27:33 +1000

    Remove legacy comment from early quest machine

--- Assets/Scripts/Game/Questing/QuestMachine.cs
+++ Assets/Scripts/Game/Questing/QuestMachine.cs
@@ -28,7 +28,6 @@ namespace DaggerfallWorkshop.Game.Questing
     /// It's possible to have the same quest multiple times (e.g. same fetch quest from two different mage guildhalls).
     /// Running quests can perform actions in the world (e.g. spawn enemies and play sounds).
     /// Or they can provide data to external systems like the NPC dialog interface (e.g. 'tell me about' and 'rumors').
-    /// Quest support is considered to be in very early prototype stages and may change at any time.
     /// </summary>
     public class QuestMachine : MonoBehaviour
     {
