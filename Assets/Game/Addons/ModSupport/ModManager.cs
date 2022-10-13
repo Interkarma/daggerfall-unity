09546c9ca372b65729d86212b036fce915039ed5
Andrew Raphael Lukasik <andrew.r.lukasik@gmail.com>
2022-10-14 00:58:37 +0200

    added: `OnDestroy()` to unload loaded asset budles

--- Assets/Game/Addons/ModSupport/ModManager.cs
+++ Assets/Game/Addons/ModSupport/ModManager.cs
@@ -158,6 +158,12 @@ namespace DaggerfallWorkshop.Game.Utility.ModSupport
             }
         }
 
+        void OnDestroy()
+        {
+            foreach (Mod mod in mods)
+                mod.UnloadAssetBundle(unloadAllObjects: true);
+        }
+
         #endregion
 
         #region Public Methods
