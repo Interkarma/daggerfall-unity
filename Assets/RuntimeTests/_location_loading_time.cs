40e7560b10defbd1b8131cee4441e703aed10d4a
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2022-10-19 23:13:45 +0200

    added: location loading time tests

---
+++ Assets/RuntimeTests/_location_loading_time.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using DaggerfallWorkshop;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Utility;
+using DaggerfallWorkshop.Utility;
+using DaggerfallWorkshop.Game.Player;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Serialization;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
+using DaggerfallWorkshop.Game.Questing;
+using DaggerfallConnect;
+using DaggerfallConnect.Utility;
+using DaggerfallConnect.Save;
+using DaggerfallWorkshop.Game.Banking;
+using DaggerfallConnect.Arena2;
+
+using Stopwatch = System.Diagnostics.Stopwatch;
+
+public class _location_loading_time
+{
+
+    [UnityTest]
+    public static IEnumerator tutorial_dungeon_loads_under_100_ms()
+    {
+        yield return RuntimeTestUtilities.LoadGameSceneRoutine();
+
+        RuntimeTestUtilities.HideMainMenuUi();
+
+        var watch = Stopwatch.StartNew();
+        {
+            RuntimeTestUtilities.LoadDungeonLocation(109, 158);
+        }
+        watch.Stop();
+
+        int ms = watch.Elapsed.Milliseconds;
+        Assert.Less(ms, 100, $"it took {ms} ms");
+
+        for (int i = 0; i < 100; i++)
+            yield return null;
+    }
+
+    [UnityTest]
+    public static IEnumerator daggerfall_city_loads_under_100_ms()
+    {
+        yield return RuntimeTestUtilities.LoadGameSceneRoutine();
+
+        RuntimeTestUtilities.HideMainMenuUi();
+
+        var watch = Stopwatch.StartNew();
+        {
+            RuntimeTestUtilities.LoadExteriorLocation(207, 213);
+        }
+        watch.Stop();
+
+        int ms = watch.Elapsed.Milliseconds;
+        Assert.Less(ms, 100, $"it took {ms} ms");
+
+        for (int i = 0; i < 100; i++)
+            yield return null;
+    }
+
+}
