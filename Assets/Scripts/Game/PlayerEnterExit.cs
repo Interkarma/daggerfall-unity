362c29cca7ab574a024ca0f77ffec7da6aa87e30
AverniteDF <115361770+AverniteDF@users.noreply.github.com>
2022-11-01 16:09:47 -0400

    Update PlayerEnterExit.cs

--- Assets/Scripts/Game/PlayerEnterExit.cs
+++ Assets/Scripts/Game/PlayerEnterExit.cs
@@ -841,9 +841,6 @@ namespace DaggerfallWorkshop.Game
             // Perform transition
             BuildingTransitionExteriorLogic();
 
-            // Immediately update ambient lighting (called again in SaveLoadManager.RestoreCachedSceneNextFrame method)
-            PlayerAmbientLight.Instance.UpdateAmbientLight(true);
-
             // Increase fade time if outside world not ready
             // This indicates a first-time transition on fresh load
             float fadeTime = 0.7f;
@@ -882,6 +879,9 @@ namespace DaggerfallWorkshop.Game
 
             // Fire event
             RaiseOnTransitionExteriorEvent();
+
+            // Immediately update ambient lighting for this frame (called again in SaveLoadManager.RestoreCachedSceneNextFrame method)
+            PlayerAmbientLight.Instance.UpdateAmbientLight();
         }
 
         #endregion
