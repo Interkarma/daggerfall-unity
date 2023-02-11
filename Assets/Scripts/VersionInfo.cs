64737cd0aa65c15d7ae65516940e2f2231e8fdbe
Interkarma <interkarma@dfworkshop.net>
2023-02-12 09:46:17 +1000

    Bump version to 0.15.1 ahead of new builds

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -16,8 +16,8 @@ public static class VersionInfo
     public const string DaggerfallUnityProductName = "Daggerfall Unity";
     public const string DaggerfallUnityStatus = "beta";
 
-    // Last updated versions 30-December-2022
-    public const string DaggerfallUnityVersion = "0.15.0";
+    // Last updated versions 12-February-2023
+    public const string DaggerfallUnityVersion = "0.15.1";
     public const string DaggerfallToolsForUnityVersion = "1.9.2";
     
     public const string BaselineUnityVersion = "2019.4.40f1";
