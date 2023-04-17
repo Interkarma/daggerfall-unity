b058866662ed9fbed1b7ecdd5778134eb1cf28f7
Interkarma <interkarma@dfworkshop.net>
2023-04-18 08:42:11 +1000

    Support larger replacement alphabets
    Very large alphabets like Hangul overflow the font atlas.
    Increased atlas texture dimension to 4096x4096 and lowered sampling size from 90 to 60.
    This has no apparent change to SDF font quality and allows for many more characters to be present in atlas before overflow.

--- Assets/Scripts/Game/UserInterface/DaggerfallFont.cs
+++ Assets/Scripts/Game/UserInterface/DaggerfallFont.cs
@@ -657,7 +657,7 @@ namespace DaggerfallWorkshop.Game.UserInterface
 
             // Create replacement TMP font asset from path
             Font font = new Font(path);
-            replacement = TMP_FontAsset.CreateFontAsset(font, 90, 9, UnityEngine.TextCore.LowLevel.GlyphRenderMode.SDFAA, 2048, 2048, AtlasPopulationMode.Dynamic);
+            replacement = TMP_FontAsset.CreateFontAsset(font, 60, 9, UnityEngine.TextCore.LowLevel.GlyphRenderMode.SDFAA, 4096, 4096, AtlasPopulationMode.Dynamic);
             if (replacement == null)
                 return false;
 
