09edb175380e37d9488112ff7bbba0f08adf7d04
Kate <katya.vesyolaya.vkv@gmail.com>
2023-09-17 03:52:32 +0300

    add health bar

---
+++ Assets/Scripts/Game/Pet/HealthBar.cs
@@ -0,0 +1,35 @@
+﻿using DaggerfallWorkshop.Game.Entity;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Pet
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image progressImage;
+        [SerializeField] DaggerfallEntityBehaviour petBehaviour;
+
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            petBehaviour = GetComponentInParent<DaggerfallEntityBehaviour>();
+        }
+
+        private void Update()
+        {
+            transform.LookAt(_mainCamera.transform);
+            transform.Rotate(0, 180, 0);
+
+            UpdateHealthBar(petBehaviour.Entity.CurrentHealthPercent);
+        }
+
+        [Button]
+        public void UpdateHealthBar(float healthPercent)
+        {
+            progressImage.fillAmount = healthPercent;
+        }
+    }
+}
