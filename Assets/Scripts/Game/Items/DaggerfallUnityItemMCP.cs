fedc92ae7b8d0b28d10e7b00743c9e85905ae052
Interkarma <interkarma@dfworkshop.net>
2023-10-12 08:08:08 +1000

    Use localized item name for recipe ingredients
    Uses template name as fallback.

--- Assets/Scripts/Game/Items/DaggerfallUnityItemMCP.cs
+++ Assets/Scripts/Game/Items/DaggerfallUnityItemMCP.cs
@@ -251,7 +251,8 @@ namespace DaggerfallWorkshop.Game.Items
                     foreach (PotionRecipe.Ingredient ingredient in potionRecipe.Ingredients)
                     {
                         ItemTemplate ingredientTemplate = DaggerfallUnity.Instance.ItemHelper.GetItemTemplate(ingredient.id);
-                        ingredientsTokens.Add(TextFile.CreateTextToken(ingredientTemplate.name));
+                        string ingredientName = TextManager.Instance.GetLocalizedItemName(ingredientTemplate.index, ingredientTemplate.name);
+                        ingredientsTokens.Add(TextFile.CreateTextToken(ingredientName));
                         ingredientsTokens.Add(TextFile.CreateFormatToken(format));
                     }
                 }
