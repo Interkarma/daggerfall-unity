469983a5fe4e47387664207fc1454cdf3c2cd8f8
Interkarma <interkarma@dfworkshop.net>
2023-11-08 07:46:36 +1000

    Bump 0.16.2 build date

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -16,7 +16,7 @@ public static class VersionInfo
     public const string DaggerfallUnityProductName = "Daggerfall Unity";
     public const string DaggerfallUnityStatus = "RC";
 
-    // Last updated versions 30-October-2023
+    // Last updated versions 08-November-2023
     public const string DaggerfallUnityVersion = "0.16.2";
     public const string DaggerfallToolsForUnityVersion = "1.9.2";
     
