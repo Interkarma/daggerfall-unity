dcb193b31c29edc9ac499ad2a5e96c8cc073ddd7
Jagget <jagget@mail.ru>
2024-03-03 13:40:17 -0500

    Use Redguard names in Redguard lands for named residences
    So, most of the Daggerfall is Breton lands. But not all. But in quests,
    we always see the named residences of Bretons. Even in the Redguard lands.
    We should use Redguard names in Redguard lands.

--- Assets/Scripts/Internal/PlayerGPS.cs
+++ Assets/Scripts/Internal/PlayerGPS.cs
@@ -427,6 +427,19 @@ namespace DaggerfallWorkshop
         }
 
         /// <summary>
+        /// Gets NameHelper.BankType in player's current region.
+        /// In practice this will always be Redguard/Breton.
+        /// Supporting other name banks for possible diversity later.
+        /// </summary>
+        public NameHelper.BankTypes GetNameBankOfRegion(int regionIndex)
+        {
+            if (regionIndex > -1)
+                return (NameHelper.BankTypes) MapsFile.RegionRaces[regionIndex];
+
+            return NameHelper.BankTypes.Breton;
+        }
+
+        /// <summary>
         /// Gets the dominant race in player's current region.
         /// </summary>
         public Races GetRaceOfCurrentRegion()
