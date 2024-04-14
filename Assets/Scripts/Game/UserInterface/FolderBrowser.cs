0bf0cecc13a9dddaa86ece11382943d1f5def4e3
kaboissonneault <kaboissonneault@gmail.com>
2024-04-14 15:13:15 -0400

    Revert changes to FolderBrowser. If a Portable Install wants an embedded DF install, use the StreamingAssets/GameFiles folder

--- Assets/Scripts/Game/UserInterface/FolderBrowser.cs
+++ Assets/Scripts/Game/UserInterface/FolderBrowser.cs
@@ -176,19 +176,6 @@ namespace DaggerfallWorkshop.Game.UserInterface
 
             // Setup initial folder conditions
             RefreshDrives();
-
-            if(DaggerfallUnityApplication.IsPortableInstall)
-            {
-				// start browsing from exe path for portable installs
-                currentPath = AppDomain.CurrentDomain.BaseDirectory;
-                if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.Windows)
-                {
-                    var drive = Path.GetPathRoot(currentPath);
-                    driveList.SelectIndexSilent(drives.FindIndex( d => d == drive ));
-                }
-                UpdatePathText();
-			}
-			
 			RefreshFolders();
 
             // Setup events
@@ -371,6 +358,7 @@ namespace DaggerfallWorkshop.Game.UserInterface
                 currentPath = newPath;
                 RefreshFolders();
                 RaisePathChangedEvent();
+
                 UpdatePathText();
             }
         }
